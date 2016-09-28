91696315a2a6c49b7e4acde7928333114f5e3fda
Jeff Brunton <j.brunton@ucl.ac.uk>
2016-09-28 10:10:55 +0100

    adding filter builder - not finished

---
+++ ER4Manager/ER4Manager_1/FilterBuilder.aspx.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.Data;
+using System.Configuration;
+using System.Collections;
+using System.Web.Security;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using System.IO;
+using System.Data.SqlClient;
+using System.Text.RegularExpressions;
+using System.Text;
+using System.Threading;
+
+public partial class FilterBuilder : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Utils.GetSessionString("Contact_ID") != null)
+        {
+            if (Utils.GetSessionString("IsAdm") == "True")
+            {
+                if (!IsPostBack)
+                {
+                    System.Web.UI.WebControls.Label lbl = (Label)Master.FindControl("lblPageTitle");
+                    if (lbl != null)
+                    {
+                        lbl.Text = "Import filter builder";
+                    }
+
+                    Telerik.Web.UI.RadTabStrip radTs = (Telerik.Web.UI.RadTabStrip)Master.FindControl("rtsMenu");
+                    if (radTs != null)
+                    {
+                        radTs.SelectedIndex = 4;
+                        radTs.Tabs[4].Tabs[8].Selected = true;
+                        //radTs.Tabs[3].Tabs[2].Width = 670;
+                    }
+                    System.Web.UI.WebControls.Label lbl1 = (Label)Master.FindControl("lblHeadingText");
+                    if (lbl1 != null)
+                    {
+                        lbl1.Text = "Import filter builder";
+                    }
+
+                    // put first call here
+                    /////
+
+                }
+            }
+            else
+            {
+                Server.Transfer("Error.aspx");
+            }
+        }
+        else
+        {
+            Server.Transfer("Error.aspx");
+        }
+    }
+}
