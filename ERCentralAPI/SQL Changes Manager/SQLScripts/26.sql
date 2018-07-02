10738142e11a87f94690dac3b026b90048537f14
Sergio Graziosi <s.graziosi@ucl.ac.uk>
2018-07-02 16:47:39 +0100

    small changes

--- ERCentralAPI/SQL Changes Manager/SQLScripts/26.sql
+++ ERCentralAPI/SQL Changes Manager/SQLScripts/26.sql
@@ -1,4 +1,40 @@
-﻿-- PATRICK SQL CHANGES
+﻿--Sergio get by ReferenceID list (not pubmed IDs)
+USE [DataService]
+GO
+/****** Object:  StoredProcedure [dbo].[st_findCitationsByExternalIDs]    Script Date: 02/07/2018 11:58:23 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE procedure [dbo].[st_findCitationsByReferenceIDs]
+(
+	@RefIDs nvarchar(max)
+)
+
+As
+
+SET NOCOUNT ON
+declare @t table (RefId bigint primary key)
+
+insert into @t (RefId) select distinct CAST([value] as bigint) from dbo.fn_Split(@RefIDs, '¬')
+
+if (@@ROWCOUNT > 0)
+BEGIN
+	select *
+	, rt.[TYPE_NAME]
+	, dbo.fn_REBUILD_AUTHORS(REFERENCE_ID, 0) as AUTHORS
+	, dbo.fn_REBUILD_AUTHORS(REFERENCE_ID, 1) as PARENT_AUTHORS
+	, dbo.fn_REBUILD_EXTERNAL_IDS(REFERENCE_ID) as EXTERNAL_IDS
+	 from TB_REFERENCE r
+	 inner join TB_REFERENCE_TYPE rt on r.TYPE_ID = rt.TYPE_ID
+	 inner join @t t on r.REFERENCE_ID = t.RefId
+END
+
+SET NOCOUNT OFF
+GO
+
+
+-- PATRICK SQL CHANGES
 -- 2 SPs altered 02/07/2018
 
 USE [DataService]
