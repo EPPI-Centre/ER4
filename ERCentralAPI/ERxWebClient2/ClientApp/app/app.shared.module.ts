e820ce4726e0f16141d2176f9152e8038bb62cc2
Sergio Graziosi <s.graziosi@ucl.ac.uk>
2018-07-16 18:24:38 +0100

    Added an extremely preliminar ERxWebClient project (API+Angular)

---
+++ ERCentralAPI/ERxWebClient2/ClientApp/app/app.shared.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+import { TreeModule } from 'angular-tree-component';
+
+import { AppComponent } from './components/app/app.component';
+import { NavMenuComponent } from './components/navmenu/navmenu.component';
+import { HomeComponent } from './components/home/home.component';
+import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
+import { CounterComponent } from './components/counter/counter.component';
+import { FetchReviewSetsComponent } from './components/fetchreviewsets/fetchreviewsets.component';
+
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        NavMenuComponent,
+        CounterComponent,
+        FetchDataComponent,
+        FetchReviewSetsComponent,
+        HomeComponent
+    ],
+    imports: [
+        CommonModule,
+        HttpModule,
+        FormsModule,
+        TreeModule,
+        RouterModule.forRoot([
+            { path: '', redirectTo: 'home', pathMatch: 'full' },
+            { path: 'home', component: HomeComponent },
+            { path: 'counter', component: CounterComponent },
+            { path: 'fetch-data', component: FetchDataComponent },
+            { path: 'fetch-reviewsets', component: FetchReviewSetsComponent },
+            { path: '**', redirectTo: 'home' }
+        ])
+    ]
+})
+export class AppModuleShared {
+}
