cb3fd7ed3480c9390b9e8dfe7fdcac545b57f98b
qtnvpod <qtnvpod@ucl.ac.uk>
2018-08-10 16:16:33 +0100

    ReadOnlyReviews as a service

--- ERCentralAPI/ERxWebClient2/ClientApp/app/components/readonlyreviews/readonlyreviews.component.ts
+++ ERCentralAPI/ERxWebClient2/ClientApp/app/components/readonlyreviews/readonlyreviews.component.ts
@@ -6,23 +6,28 @@ import { Router } from '@angular/router';
 import { Observable, } from 'rxjs';
 import { ReviewerIdentityService } from '../services/revieweridentity.service';
 import { ReviewerIdentity } from '../services/revieweridentity.service';
-
+import { readonlyreviewsService } from '../services/readonlyreviews.service';
 
 
 @Component({
     selector: 'readonlyreviews',
     templateUrl: './readonlyreviews.component.html',
-    //providers: [ReviewerIdentityService]
     providers: []
 })
 export class FetchReadOnlyReviewsComponent implements OnInit {
-    public ReviewList: ReadOnlyReview[] = [];
-    constructor(private router: Router, private _httpC: HttpClient, @Inject('BASE_URL') private _baseUrl: string, private ReviewerIdentityServ: ReviewerIdentityService) {
-        console.log('rOr constructor: ' + this.ReviewerIdentityServ.reviewerIdentity.userId);
+
+    constructor(private router: Router,
+                private _httpC: HttpClient,
+                @Inject('BASE_URL') private _baseUrl: string,
+                private ReviewerIdentityServ: ReviewerIdentityService,
+                private _readonlyreviewsService: readonlyreviewsService) {
+
+                console.log('rOr constructor: ' + this.ReviewerIdentityServ.reviewerIdentity.userId);
     }
+
     onSubmit(f: string) {
-        let RevId: number = parseInt(f, 10);
-        this.ReviewerIdentityServ.LoginToReview(RevId).subscribe(ri => {
+            let RevId: number = parseInt(f, 10);
+            this.ReviewerIdentityServ.LoginToReview(RevId).subscribe(ri => {
             this.ReviewerIdentityServ.reviewerIdentity = ri;
             console.log('login to Review: ' + this.ReviewerIdentityServ.reviewerIdentity.userId);
             if (this.ReviewerIdentityServ.reviewerIdentity.userId > 0 && this.ReviewerIdentityServ.reviewerIdentity.reviewId === RevId) {
@@ -34,42 +39,52 @@ export class FetchReadOnlyReviewsComponent implements OnInit {
         //this.router.navigate(['fetch-reviewsets'])
     }
 
-    getReviews() {
+    //getReviews() {
 
-        let body2 = "contactId=" + this.ReviewerIdentityServ.reviewerIdentity.userId;
-        console.log('Calling get reviews...!');
-         this._httpC.post<ReadOnlyReview[]>(this._baseUrl + 'api/review/reviewsbycontact',
-             body2).subscribe(result => this.ReviewList = result);
+    //    let body2 = "contactId=" + this.ReviewerIdentityServ.reviewerIdentity.userId;
+    //    console.log('Calling get reviews...!');
+    //     this._httpC.post<ReadOnlyReview[]>(this._baseUrl + 'api/review/reviewsbycontact',
+    //         body2).subscribe(result => this.ReviewList = result);
+    //}
+
+    //ngOnInit() {
+
+    //    if (this.ReviewerIdentityServ.reviewerIdentity.userId == 0) {
+    //        console.log("didn't work!");
+    //        this.router.navigate(['home']);
+    //    }
+    //    else {
+
+    //        this.ReviewerIdentityServ.Report();
+    //        this.getReviews();//we don't want this sort of thing in the constructor, API calls should be done after...
+    //    }
+
+      
+    //}
+
+    getReviews() {
+        console.log('inside get reviews');
+        if (this._readonlyreviewsService.ReadOnlyReviews.length == 0) {
+            this._readonlyreviewsService.Fetch().subscribe(result => {
+                this._readonlyreviewsService.ReadOnlyReviews = result;
+                console.log("got " + this._readonlyreviewsService.ReadOnlyReviews.length + " ReadOnlyReviews.");
+            });
+        }
     }
+
     ngOnInit() {
 
         if (this.ReviewerIdentityServ.reviewerIdentity.userId == 0) {
-            console.log("didn't work!");
+            console.log('user is empty...');
             this.router.navigate(['home']);
         }
         else {
 
-            //const userJson = localStorage.getItem('currentErUser');
-
-            //console.log('On intiation this should have th user from the localstorage ' + userJson);
-
-            //let currentUser: any = userJson !== null ? JSON.parse(userJson) : '';
-
-            //console.log('rOr init: ' + currentUser.userId);
+            console.log("getting ReadOnlyReviews");
             this.ReviewerIdentityServ.Report();
-            this.getReviews();//we don't want this sort of thing in the constructor, API calls should be done after...
+            this.getReviews();
         }
-
-      
     }
+
+
 }
-
-
-interface ReadOnlyReview {
-    reviewId: number;
-    reviewName: string;
-    contactReviewRoles: string;
-    reviewOwner: string;
-    lastAccess: string;
-}
-
--- ERCentralAPI/ERxWebClient2/Controllers/ReviewController.cs
+++ ERCentralAPI/ERxWebClient2/Controllers/ReviewController.cs
@@ -7,6 +7,7 @@ using System.Security.Principal;
 using System.Threading.Tasks;
 using BusinessLibrary.BusinessClasses;
 using BusinessLibrary.Security;
+using Csla;
 using Csla.Data;
 using ERxWebClient2.Models;
 using Microsoft.AspNetCore.Authorization;
@@ -15,48 +16,73 @@ using Newtonsoft.Json.Linq;
 
 namespace ERxWebClient2.Controllers
 {
+    //[Authorize]
+    //[Route("api/[controller]")]
+    //public class ReviewController : CSLAController
+    //{
+    //    [HttpPost("[action]")]
+    //    public IEnumerable<ReadOnlyReview> ReviewsByContact(int contactId)//should receive a reviewID!
+    //    {
+    //        SetCSLAUser();
+    //        ReadOnlyReviewList returnValue = new ReadOnlyReviewList();
+    //        ReadOnlyReviewList.GetReviewList(contactId, (o, e) =>
+    //        {
+    //            if (e.Error == null)
+    //            {
+    //                returnValue = e.Object;
+    //            }
+    //        });
+
+
+
+    //        List<ReadOnlyReview> res = new List<ReadOnlyReview>();
+    //        using (SqlConnection conn = new SqlConnection(Program.SqlHelper.ER4DB))
+    //        {
+    //            SqlParameter cid = new SqlParameter("@CONTACT_ID", contactId);
+    //            try
+    //            {
+    //                using (SafeDataReader reader = Program.SqlHelper.ExecuteCSLAQuerySP(conn, "st_ReviewContact", cid))
+    //                {
+    //                    if (reader != null)
+    //                    {
+    //                        while (reader.Read()) res.Add(ReadOnlyReview.GetReadOnlyReview(reader));
+    //                    }
+    //                }
+    //            }
+    //            catch (Exception e)
+    //            {
+    //                Program.Logger.LogSQLException(e, "Error fetching list of codesets", cid);
+    //            }
+    //        }
+    //        IEnumerable<ReviewSet> res2 = res as IEnumerable<ReviewSet>;
+    //        return (IEnumerable<ReadOnlyReview>)res;
+    //    }
+
+
+    //}
+
     [Authorize]
     [Route("api/[controller]")]
     public class ReviewController : CSLAController
     {
-        [HttpPost("[action]")]
-        public IEnumerable<ReadOnlyReview> ReviewsByContact(int contactId)//should receive a reviewID!
+
+        [HttpGet("[action]")]
+        public ReadOnlyReviewList ReadOnlyReviews()//should receive a reviewID!
         {
             SetCSLAUser();
-            ReadOnlyReviewList returnValue = new ReadOnlyReviewList();
-            ReadOnlyReviewList.GetReviewList(contactId, (o, e) =>
-            {
-                if (e.Error == null)
-                {
-                    returnValue = e.Object;
-                }
-            });
+            ReviewerIdentity ri = Csla.ApplicationContext.User.Identity as ReviewerIdentity;
 
+            DataPortal<ReadOnlyReviewList> dp = new DataPortal<ReadOnlyReviewList>();
+            SingleCriteria<ReadOnlyReviewList, int> criteria = new SingleCriteria<ReadOnlyReviewList, int>(ri.UserId);
+            ReadOnlyReviewList result = dp.Fetch(criteria);
 
+            //ReadOnlyReviewList returnValue = new ReadOnlyReviewList();
+            //Action<ReviewerIdentity, ReadOnlyReviewList> Action = new Action<ReviewerIdentity, ReadOnlyReviewList>(Doit);
+            //Action.Invoke(ri, returnValue);
 
-            List<ReadOnlyReview> res = new List<ReadOnlyReview>();
-            using (SqlConnection conn = new SqlConnection(Program.SqlHelper.ER4DB))
-            {
-                SqlParameter cid = new SqlParameter("@CONTACT_ID", contactId);
-                try
-                {
-                    using (SafeDataReader reader = Program.SqlHelper.ExecuteCSLAQuerySP(conn, "st_ReviewContact", cid))
-                    {
-                        if (reader != null)
-                        {
-                            while (reader.Read()) res.Add(ReadOnlyReview.GetReadOnlyReview(reader));
-                        }
-                    }
-                }
-                catch (Exception e)
-                {
-                    Program.Logger.LogSQLException(e, "Error fetching list of codesets", cid);
-                }
-            }
-            IEnumerable<ReviewSet> res2 = res as IEnumerable<ReviewSet>;
-            return (IEnumerable<ReadOnlyReview>)res;
+            //return returnValue;
+            return result;
         }
-
         
     }
 }
