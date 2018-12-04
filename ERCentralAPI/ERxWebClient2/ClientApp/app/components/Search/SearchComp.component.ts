4a4a110e663d1e3c6594ce60d3ff8af688986fe1
Patrick ODriscoll <qtnvpod@outlook.com>
2018-12-04 11:15:16 +0000

    Search draft out of modal.  Lots to do. (D100%)

--- ERCentralAPI/ERxWebClient2/ClientApp/app/components/Search/SearchComp.component.ts
+++ ERCentralAPI/ERxWebClient2/ClientApp/app/components/Search/SearchComp.component.ts
@@ -31,16 +31,44 @@ export class SearchComp implements OnInit, OnDestroy {
 		public _searchService: searchService,
 		private _eventEmitter: EventEmitterService,
 		private modalService: ModalSearchService,
-		//public activeModal: NgbActiveModal,
 		private reviewSetsService: ReviewSetsService
 	) {
 	}
 
 	@ViewChild('testKendoGrid') searchesGridRes!: GridComponent;
 
-	//public _searchesModalContent: SearchesModalContent = new
-	//	SearchesModalContent(this.activeModal, this.reviewSetsService, this._searchService);
-	
+	private _searchInclOrExcl: string = 'true';
+	public get searchInclOrExcl(): string {
+		console.log('I get it', this._searchInclOrExcl);
+		return this._searchInclOrExcl;
+	}
+	public set searchInclOrExcl(value: string) {
+		if (value == 'true' || value == 'false') this._searchInclOrExcl = value;
+		else console.log("I'm not doing it :-P ", value);
+	}
+
+
+	private canWrite: boolean = true;
+	public dropDownList: any = null;
+	public showTextBox: boolean = false;
+	public selectedSearchDropDown: string = '';
+	public selectedSearchTextDropDown: string = '';
+	public selectedSearchCodeSetDropDown: string = '';
+	public CodeSets: any[] = [];
+
+
+	_setID: number = 0;
+	_searchText: string = '';
+	_title: string = '';
+	_answers: string = '';
+	_included: boolean = true;
+	_withCodes: boolean = false;;
+	_searchId: number = 0;
+	_IDs: string = '';
+
+	public get IsReadOnly(): boolean {
+		return this.canWrite;
+	}
     public selectedAll: boolean = false;
 	public modalClass: boolean = false;
 
@@ -116,28 +144,205 @@ export class SearchComp implements OnInit, OnDestroy {
 		console.log(lstStrSearchIds);
 		this._searchService.Delete(lstStrSearchIds);
 	}
-	
-	//openNewSearchModal() {
-				
+	public cmdSearches: SearchCodeCommand = {
+		_setID: 0,
+		_searchText: '',
+		_IDs: '',
+		_title: '',
+		_answers: '',
+		_included: false,
+		_withCodes: false,
+		_searchId: 0
+	};
 
-	//	let modalNew = this.modalService.open(SearchesModalContent, { size: 'lg', centered: true });
-	//	modalNew.componentInstance.InfoBoxTextInput = 'tester';
-	//	modalNew.componentInstance.focus(null);
+	public withCode: boolean = false;
+	public attributeNames: string = '';
+	public commaIDs: string = '';
+	public searchText: string = '';
 
-	//	modalNew.result.then(
+	callSearches(selectedSearchDropDown: string, selectedSearchTextDropDown: string, searchBool: boolean) {
 
-	//		//called if search button is pressed
-	//		(result) => {
+		this.selectedSearchTextDropDown = selectedSearchTextDropDown;
+		let searchTitle: string = '';
+		let firstNum: boolean = selectedSearchDropDown.search('With this code') != -1;
+		let secNum: boolean = selectedSearchDropDown.search('Without this code') != -1
+		this.cmdSearches._included = Boolean(searchBool);
+		this.cmdSearches._withCodes = this.withCode;
+		this.cmdSearches._searchId = 0;
 
-	//			console.log('pressed okay: ' + JSON.stringify(result));
+		if (firstNum == true || secNum == true) {
 
-	//		},
-	//		//called if modal is cancelled
-	//		(result) => {
-	//			console.log('cancelled: ' + JSON.stringify(result));
-	//		}
-	//	);
-	//}
+			if (firstNum) {
+
+				this.withCode = true;
+			} else {
+
+				this.withCode = false;
+			}
+
+			if (this.reviewSetsService.selectedNode != undefined) {
+
+				let tmpID: number = this.reviewSetsService.selectedNode.attributeSetId;
+				this.attributeNames = this.reviewSetsService.selectedNode.name;
+				this.cmdSearches._answers = String(tmpID);
+				alert(this.reviewSetsService.selectedNode);
+
+				searchTitle = this.withCode == true ?
+					"Coded with: " + this.attributeNames : "Not coded with: " + this.attributeNames;
+
+
+				this.cmdSearches._title = searchTitle;
+				this.cmdSearches._withCodes = this.withCode;
+
+				this._searchService.CreateSearch(this.cmdSearches, 'SearchCodes');
+
+			}
+		}
+
+		if (selectedSearchDropDown == 'With these internal IDs (comma separated)') {
+
+			this.cmdSearches._IDs = this.commaIDs;
+			this.cmdSearches._title = this.commaIDs;
+			this._searchService.CreateSearch(this.cmdSearches, 'SearchIDs');
+
+
+		}
+		if (selectedSearchDropDown == 'With these imported IDs (comma separated)') {
+
+			this.cmdSearches._IDs = this.commaIDs;
+			this.cmdSearches._title = this.commaIDs;
+
+			this._searchService.CreateSearch(this.cmdSearches, 'SearchImportedIDs');
+
+
+		}
+		if (selectedSearchDropDown == 'Containing this text') {
+
+			this.cmdSearches._title = this.searchText;
+			this.cmdSearches._included = Boolean(searchBool);
+			this._searchService.CreateSearch(this.cmdSearches, 'SearchText');
+		}
+		if (selectedSearchDropDown == 'That have at least one code from this set') {
+
+			this.cmdSearches._withCodes = true;
+			this.cmdSearches._title = this.selectedSearchCodeSetDropDown;
+
+			this._searchService.CreateSearch(this.cmdSearches, 'SearchCodeSetCheck');
+
+		}
+		if (selectedSearchDropDown == 'That dont have any codes from this set') {
+
+			this.cmdSearches._withCodes = false;
+			this.cmdSearches._title = this.selectedSearchCodeSetDropDown;
+			this._searchService.CreateSearch(this.cmdSearches, 'SearchCodeSetCheck');
+
+		}
+		if (selectedSearchDropDown == 'Without an abstract') {
+
+			alert(selectedSearchDropDown);
+			this.cmdSearches._title = searchTitle;
+
+			this._searchService.CreateSearch(this.cmdSearches, 'SearchNoAbstract');
+
+		}
+
+		if (selectedSearchDropDown == 'Without any documents uploaded') {
+
+			alert(selectedSearchDropDown);
+			this.cmdSearches._title = 'Without any documents uploaded';
+
+			this._searchService.CreateSearch(this.cmdSearches, 'SearchNoFiles');
+
+		}
+		if (selectedSearchDropDown == 'With at least one document uploaded') {
+
+			this.cmdSearches._title = 'With at least one document uploaded.';
+			this._searchService.CreateSearch(this.cmdSearches, 'SearchOneFile');
+
+		}
+
+	}
+
+	public nextDropDownList(num: number, val: string) {
+
+		this.showTextBox = false;
+		this.selectedSearchDropDown = val;
+
+		switch (num) {
+
+			case 1: {
+				this.dropDownList = this.reviewSetsService.ReviewSets;
+				break;
+			}
+			case 2: {
+				this.dropDownList = this.reviewSetsService.ReviewSets;
+				break;
+			}
+			case 3: {
+				this.showTextBox = true;
+				break;
+			}
+			case 4: {
+				this.showTextBox = true;
+				break;
+			}
+			case 5: {
+
+				this.CodeSets = this.reviewSetsService.ReviewSets.filter(x => x.nodeType == 'ReviewSet')
+					.map(
+						(y: ReviewSet) => {
+							return y.name;
+						}
+					);
+				this.dropDownList = this.reviewSetsService.ReviewSets;
+				break;
+			}
+			case 6: {
+				this.CodeSets = this.reviewSetsService.ReviewSets.filter(x => x.nodeType == 'ReviewSet')
+					.map(
+						(y: ReviewSet) => {
+							return y.name;
+						}
+					);
+				this.dropDownList = this.reviewSetsService.ReviewSets;
+				break;
+			}
+			case 7: {
+				break;
+			}
+			case 8: {
+				break;
+			}
+			case 9: {
+				break;
+			}
+			case 10: {
+				break;
+			}
+			default: {
+				break;
+			}
+		}
+	}
+
+	public setSearchCodeSetDropDown(codeSetName: string) {
+
+		this.selectedSearchCodeSetDropDown = this.reviewSetsService.ReviewSets.filter(x => x.name == codeSetName)
+			.map(
+				(y: ReviewSet) => {
+
+					this.cmdSearches._setID = y.set_id;
+					return y.name;
+				}
+			)[0];
+	}
+
+	public setSearchTextDropDown(heading: string) {
+
+		this.selectedSearchTextDropDown = heading;
+		this.cmdSearches._searchText = heading;
+	}
+
 	
 	public checkboxClicked(dataItem: any) {
 
@@ -192,7 +397,8 @@ export class SearchComp implements OnInit, OnDestroy {
 			this.router.navigate(['home']);
 		}
 		else {
-			this.bodyText = 'This text can be updated in modal 1';
+
+			this._searchInclOrExcl = 'true';
             this._searchService.Fetch();
 
 		}
