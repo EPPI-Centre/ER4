d9109016ba66ba0dca390640b753037dcd389e45
Patrick ODriscoll <qtnvpod@outlook.com>
2018-12-04 11:56:42 +0000

    Tweak

--- ERCentralAPI/ERxWebClient2/ClientApp/app/components/services/search.service.ts
+++ ERCentralAPI/ERxWebClient2/ClientApp/app/components/services/search.service.ts
@@ -1,4 +1,4 @@
-import {  Inject, Injectable, EventEmitter, Output} from '@angular/core';
+import {  Inject, Injectable} from '@angular/core';
 import { HttpClient   } from '@angular/common/http';
 import { ModalService } from './modal.service';
 
