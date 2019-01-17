e2c8094f2fa07a0825ec71effe21d872c012e046
Patrick ODriscoll <qtnvpod@outlook.com>
2019-01-17 14:57:11 +0000

    Searches tweaks from list.

--- ERCentralAPI/ERxWebClient2/ClientApp/app/components/ConfirmationDialog/confirmation-dialog.component.ts
+++ ERCentralAPI/ERxWebClient2/ClientApp/app/components/ConfirmationDialog/confirmation-dialog.component.ts
@@ -4,7 +4,6 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 @Component({
 	selector: 'app-confirmation-dialog',
 	templateUrl: './confirmation-dialog.component.html',
-	//styleUrls: ['./confirmation-dialog.component.scss'],
 })
 export class ConfirmationDialogComponent implements OnInit {
 
--- ERCentralAPI/ERxWebClient2/ClientApp/app/components/services/confirmation-dialog.service.ts
+++ ERCentralAPI/ERxWebClient2/ClientApp/app/components/services/confirmation-dialog.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ConfirmationDialogComponent } from '../ConfirmationDialog/confirmation-dialog.component';
 
