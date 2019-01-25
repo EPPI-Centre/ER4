18aebcae55a1568003414fb3c75a540be9622ab2
Sergio Graziosi <s.graziosi@ucl.ac.uk>
2019-01-25 16:48:45 +0000

    Bugfix: Special (regex) characters in the "terms list" break the coding pages.

--- ERCentralAPI/ERxWebClient2/ClientApp/app/components/itemDetails/itemDetails.component.ts
+++ ERCentralAPI/ERxWebClient2/ClientApp/app/components/itemDetails/itemDetails.component.ts
@@ -6,6 +6,7 @@ import { Observable } from 'rxjs';
 import { Item } from '../services/ItemList.service';
 import { ReviewerTermsService } from '../services/ReviewerTerms.service';
 import { ItemDocsService } from '../services/itemdocs.service';
+import { ModalService } from '../services/modal.service';
 
 
 
@@ -18,7 +19,8 @@ import { ItemDocsService } from '../services/itemdocs.service';
 export class itemDetailsComp implements OnInit {
 
     constructor(private ReviewerTermsService: ReviewerTermsService,
-        public ItemDocsService: ItemDocsService
+        public ItemDocsService: ItemDocsService,
+        private ModalService: ModalService
     ) {}
 
     @Input() item: Item | undefined;
@@ -43,31 +45,54 @@ export class itemDetailsComp implements OnInit {
             this.HTitle = this.item.title;
             this.HAbstract = this.item.abstract;
             for (let term of this.ReviewerTermsService.TermsList) {
-                if (term.reviewerTerm && term.reviewerTerm.length > 0) {
-                   
-                    let lFirst = term.reviewerTerm.substr(0, 1);
-                    lFirst = lFirst.toLowerCase();
-                    let uFirst = lFirst.toUpperCase();
-                    let lTerm = lFirst + term.reviewerTerm.substr(1);
-                    let uTerm = uFirst + term.reviewerTerm.substr(1);
-                  
-                    let reg = new RegExp(lTerm, "g");
-                    let reg2 = new RegExp(uTerm, "g");
-                    if (term.included) {
-                        this.HTitle = this.HTitle.replace(reg, "<span class='RelevantTerm'>" + lTerm + "</span>");
-                        this.HTitle = this.HTitle.replace(reg2, "<span class='RelevantTerm'>" + uTerm + "</span>");
-                        this.HAbstract = this.HAbstract.replace(reg, "<span class='RelevantTerm'>" + lTerm + "</span>");
-                        this.HAbstract = this.HAbstract.replace(reg2, "<span class='RelevantTerm'>" + uTerm + "</span>");
+                try {
+                    if (term.reviewerTerm && term.reviewerTerm.length > 0) {
+                        let lFirst = term.reviewerTerm.substr(0, 1);
+                        lFirst = lFirst.toLowerCase();
+                        let uFirst = lFirst.toUpperCase();
+                        let lTerm = lFirst + term.reviewerTerm.substr(1);
+                        let uTerm = uFirst + term.reviewerTerm.substr(1);
+
+                        let reg = new RegExp(this.cleanSpecialRegexChars(lTerm), "g");
+                        let reg2 = new RegExp(this.cleanSpecialRegexChars(uTerm), "g");
+                        if (term.included) {
+                            this.HTitle = this.HTitle.replace(reg, "<span class='RelevantTerm'>" + lTerm + "</span>");
+                            this.HTitle = this.HTitle.replace(reg2, "<span class='RelevantTerm'>" + uTerm + "</span>");
+                            this.HAbstract = this.HAbstract.replace(reg, "<span class='RelevantTerm'>" + lTerm + "</span>");
+                            this.HAbstract = this.HAbstract.replace(reg2, "<span class='RelevantTerm'>" + uTerm + "</span>");
+                        }
+                        else {
+                            this.HTitle = this.HTitle.replace(reg, "<span class='IrrelevantTerm'>" + lTerm + "</span>");
+                            this.HTitle = this.HTitle.replace(reg2, "<span class='IrrelevantTerm'>" + uTerm + "</span>");
+                            this.HAbstract = this.HAbstract.replace(reg, "<span class='IrrelevantTerm'>" + lTerm + "</span>");
+                            this.HAbstract = this.HAbstract.replace(reg2, "<span class='IrrelevantTerm'>" + uTerm + "</span>");
+                        }
                     }
-                    else {
-                        this.HTitle = this.HTitle.replace(reg, "<span class='IrrelevantTerm'>" + lTerm + "</span>");
-                        this.HTitle = this.HTitle.replace(reg2, "<span class='IrrelevantTerm'>" + uTerm + "</span>");
-                        this.HAbstract = this.HAbstract.replace(reg, "<span class='IrrelevantTerm'>" + lTerm + "</span>");
-                        this.HAbstract = this.HAbstract.replace(reg2, "<span class='IrrelevantTerm'>" + uTerm + "</span>");
-                    }
+                }
+                catch (error) {
+                    console.log(error);
+                    this.ModalService.GenericErrorMessage("Sorry, the terms-highlighting system has encountered a problem. Please inform EPPI-Support.");
                 }
             }
         }
+    }
+    private cleanSpecialRegexChars(input: string): string {
+        //need to replace these: [\^$.|?*+(){}
+        let result = input.replace(/\[/g, "\\[");
+        result = result.replace(/\^/g, "\\^");
+        result = result.replace(/\\/g, "\\");
+        result = result.replace(/\$/g, "\\$");
+        result = result.replace(/\./g, "\\.");
+        result = result.replace(/\|/g, "\\|");
+        result = result.replace(/\?/g, "\\?");
+        result = result.replace(/\*/g, "\\*");
+        result = result.replace(/\+/g, "\\+");
+        result = result.replace(/\(/g, "\\(");
+        result = result.replace(/\)/g, "\\)");
+        result = result.replace(/\{/g, "\\{");
+        result = result.replace(/\}/g, "\\}");
+        console.log(input, result);
+        return result;
     }
     toHTML(text: string): string {
         return text.replace(/\r\n/g, '<br />').replace(/\r/g, '<br />').replace(/\n/g, '<br />');
