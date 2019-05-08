e6a61824005a894cfff4dc5ea83e8cdde39dff8a
Patrick ODriscoll <qtnvpod@outlook.com>
2019-05-08 13:51:21 +0100

    Reconciliation Comparison refactoring

--- ERCentralAPI/ERxWebClient2/ClientApp/app/components/Comparison/comparisonreconciliation.component.ts
+++ ERCentralAPI/ERxWebClient2/ClientApp/app/components/Comparison/comparisonreconciliation.component.ts
@@ -1,6 +1,5 @@
 import { Component,  OnInit } from '@angular/core';
-import { ReviewSetsService, SetAttribute, ReviewSet } from '../services/ReviewSets.service';
-import { ReviewInfoService } from '../services/ReviewInfo.service';
+import { ReviewSetsService, ReviewSet } from '../services/ReviewSets.service';
 import { ComparisonsService, Comparison } from '../services/comparisons.service';
 import { Router } from '@angular/router';
 import { ItemListService, Item } from '../services/ItemList.service';
@@ -19,7 +18,6 @@ export class ComparisonReconciliationComp implements OnInit {
 	constructor(
 		private router: Router, 
 		private _reviewSetsService: ReviewSetsService,
-		private _reviewInfoService: ReviewInfoService,
 		private _ItemListService: ItemListService,
 		private _comparisonsService: ComparisonsService,
 		private _reconciliationService: ReconciliationService,
@@ -38,28 +36,29 @@ export class ComparisonReconciliationComp implements OnInit {
     public get CodeSets(): ReviewSet[] {
 		return this._reviewSetsService.ReviewSets.filter(x => x.setType.allowComparison != false);
     }
-
 	getReconciliations() {
 
 		if (this.item != null && this.item != undefined) {
 			this.CurrentComparison = this._comparisonsService.currentComparison;
-			this.ReviewSet = this._reviewSetsService.GetReviewSets().filter(
-				x => x.set_id == this.CurrentComparison.setId)[0];
-			this.localList = new ReconcilingItemList(this.ReviewSet,
-				this.CurrentComparison, "testing right now");
-			let i: number = 0;
-			this.recursiveItemList(i);
+			if (this.CurrentComparison) {
+				this.ReviewSet = this._reviewSetsService.GetReviewSets().filter(
+					x => x.set_id == this.CurrentComparison.setId)[0];
+				this.localList = new ReconcilingItemList(this.ReviewSet,
+					this.CurrentComparison, "");
+				let i: number = 0;
+					this.recursiveItemList(i);
+			}
 		}
 	}
 	recursiveItemList(i: number) {
-		let ItemSetListTest: ItemSet[] = [];
+		let ItemSetlst: ItemSet[] = [];
 		this._reconciliationService.FetchItemSetList(this._ItemListService.ItemList.items[i].itemId)
 
 			.then(
 				(res: ItemSet[]) => {
 
-					ItemSetListTest = res;
-					this.localList.AddItem(this._ItemListService.ItemList.items[i], ItemSetListTest);
+					ItemSetlst = res;
+					this.localList.AddItem(this._ItemListService.ItemList.items[i], ItemSetlst);
 					if (i < this._ItemListService.ItemList.items.length-1) {
 						i = i + 1;
 						this.recursiveItemList(i);
@@ -90,27 +89,30 @@ export class ComparisonReconciliationComp implements OnInit {
 	}
 	public UnComplete(recon: ReconcilingItem) {
 
-		this._reconciliationService.ItemSetCompleteComparison(recon, this.CurrentComparison, 0, false)
-			.then(
-				() => {
-					this.RefreshData();
-				}
-			);
+		if (recon && this.CurrentComparison) {
+			this._reconciliationService.ItemSetCompleteComparison(recon, this.CurrentComparison, 0, false)
+				.then(
+					() => {
+						this.RefreshData();
+					}
+				);
+		}
 	}
 	public Complete(recon: ReconcilingItem, contactID: number) {
-
-		this._reconciliationService.ItemSetCompleteComparison(recon, this.CurrentComparison, contactID, true)
-			.then(
-				() => {
-					this.RefreshData();
-				}
-			);
+		if (recon && this.CurrentComparison) {
+			this._reconciliationService.ItemSetCompleteComparison(recon, this.CurrentComparison, contactID, true)
+				.then(
+					() => {
+						this.RefreshData();
+					}
+				);
+		}
 	}
 	public reconcilingCodeArrayLength(len: number): any {
 
 		return Array.from({ length: len }, (v, k) => k + 1);
 	}
-	public ReconRowClick(itemid: number) {
+	public ChangePanelItem(itemid: number) {
 
 		let tempItemList = this._ItemListService.ItemList.items;
 		this.panelItem = tempItemList.find(x => x.itemId == itemid);
