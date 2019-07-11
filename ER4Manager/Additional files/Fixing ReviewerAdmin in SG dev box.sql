0756133195fa20453ac6922e47e0bd5e757482d0
Sergio Graziosi <s.graziosi@ucl.ac.uk>
2019-07-11 11:13:15 +0100

    One more missing SP from revieweradmin (on SG dev box!). Enforce Read-Only on all reviewstats.

--- ER4Manager/Additional files/Fixing ReviewerAdmin in SG dev box.sql
+++ ER4Manager/Additional files/Fixing ReviewerAdmin in SG dev box.sql
@@ -5,7 +5,9 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[st_ReviewMembers_2]') AND type in (N'P', N'PC'))
+DROP PROCEDURE [dbo].[st_ReviewMembers_2]
+GO
 
 -- =============================================
 -- Author:		<Jeff>
@@ -114,3 +116,169 @@ BEGIN
 END
 GO
 
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[st_ContactDetailsFullCreateOrEdit]') AND type in (N'P', N'PC'))
+DROP PROCEDURE [dbo].[st_ContactDetailsFullCreateOrEdit]
+GO
+
+
+CREATE procedure [dbo].[st_ContactDetailsFullCreateOrEdit]
+(
+	@NEW_ACCOUNT bit,
+	@CONTACT_NAME nvarchar(255),
+	@USERNAME nvarchar(50),
+	@PASSWORD varchar(50),
+	@DATE_CREATED datetime,
+	@EXPIRY_DATE date,
+	@CREATOR_ID int,
+	@EMAIL nvarchar(500),
+	@DESCRIPTION nvarchar(1000),
+	@IS_SITE_ADMIN bit,
+	@CONTACT_ID nvarchar(50), -- it might say 'New'
+	@EXTENSION_TYPE_ID int,
+	@EXTENSION_NOTES nvarchar(500),
+	@EDITOR_ID int,
+	@MONTHS_CREDIT nvarchar(50),
+	@ARCHIE_ID varchar(32),
+	@RESULT nvarchar(50) output
+)
+
+
+As
+
+SET NOCOUNT ON
+DECLARE @ORIGINAL_EXPIRY datetime
+DECLARE @NEW_ROW_ID int
+
+if @EXPIRY_DATE = ''
+begin
+	SET @EXPIRY_DATE = NULL
+end
+
+BEGIN TRY
+
+BEGIN TRANSACTION
+	if @NEW_ACCOUNT = 1
+	begin
+				
+		-- create a new account
+		INSERT INTO Reviewer.dbo.TB_CONTACT(CONTACT_NAME, USERNAME, /*[PASSWORD],*/ 
+			DATE_CREATED, [EXPIRY_DATE], EMAIL, [DESCRIPTION], CREATOR_ID, MONTHS_CREDIT)
+		VALUES (@CONTACT_NAME, @USERNAME, /*@PASSWORD,*/ @DATE_CREATED, @EXPIRY_DATE,
+			@EMAIL, @DESCRIPTION, @CREATOR_ID, @MONTHS_CREDIT)
+	
+		set @RESULT = @@IDENTITY
+		if (@CREATOR_ID != @CONTACT_ID)
+		begin
+			update Reviewer.dbo.TB_CONTACT 
+			set CREATOR_ID = @RESULT
+			where CONTACT_ID = @RESULT
+			and USERNAME = @USERNAME
+		end
+		
+		DECLARE @chars char(100) = '!т#$%&а()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~ийклм'
+		declare @rnd varchar(20)
+		declare @cnt int = 0
+		set @rnd = ''
+		WHILE (@cnt <= 20) 
+		BEGIN
+			SELECT @rnd = @rnd + 
+				SUBSTRING(@chars, CONVERT(int, RAND() * 100), 1)
+			SELECT @cnt = @cnt + 1
+		END
+		update Reviewer.dbo.TB_CONTACT 
+		set FLAVOUR = @rnd, PWASHED = HASHBYTES('SHA1', @PASSWORD + @rnd)
+		where CONTACT_ID = @RESULT
+		
+		
+		
+    end
+	else  -- edit an existing account
+	begin	
+		-- get original expiry date from TB_CONTACT
+		select @ORIGINAL_EXPIRY = c.EXPIRY_DATE 
+		from Reviewer.dbo.TB_CONTACT c
+		where c.CONTACT_ID = @CONTACT_ID
+		
+		--update TB_CONTACT
+		update Reviewer.dbo.TB_CONTACT 
+		set 
+		CONTACT_NAME = @CONTACT_NAME,
+		USERNAME = @USERNAME,
+		/*[PASSWORD] = @PASSWORD,*/
+		DATE_CREATED = @DATE_CREATED,
+		[EXPIRY_DATE] = @EXPIRY_DATE,
+		CREATOR_ID = @CREATOR_ID,
+		EMAIL = @EMAIL,
+		[DESCRIPTION] = @DESCRIPTION,
+		IS_SITE_ADMIN = @IS_SITE_ADMIN,
+		MONTHS_CREDIT = @MONTHS_CREDIT,
+		ARCHIE_ID = @ARCHIE_ID
+		where CONTACT_ID = @CONTACT_ID
+		
+		-- to get the null value correct
+		if @ARCHIE_ID = ''
+		begin
+			update Reviewer.dbo.TB_CONTACT 
+			set ARCHIE_ID = null
+			where CONTACT_ID = @CONTACT_ID
+		end
+		
+		
+		
+		if (@PASSWORD != '')
+		begin
+			DECLARE @chars1 char(100) = '!т#$%&а()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~ийклм'
+			declare @rnd1 varchar(20)
+			declare @cnt1 int = 0
+			set @rnd1 = ''
+			WHILE (@cnt1 <= 20) 
+			BEGIN
+				SELECT @rnd1 = @rnd1 + 
+					SUBSTRING(@chars1, CONVERT(int, RAND() * 100), 1)
+				SELECT @cnt1 = @cnt1 + 1
+			END
+			update Reviewer.dbo.TB_CONTACT 
+			set FLAVOUR = @rnd1, PWASHED = HASHBYTES('SHA1', @PASSWORD + @rnd1)
+			where CONTACT_ID = @CONTACT_ID
+		end
+		
+		
+		if (@EXTENSION_TYPE_ID > 1)
+		begin
+			-- create new row in TB_EXPIRY_EDIT_LOG -- using bogus old expiry date
+			insert into TB_EXPIRY_EDIT_LOG
+			(DATE_OF_EDIT, TYPE_EXTENDED, ID_EXTENDED, NEW_EXPIRY_DATE,
+			OLD_EXPIRY_DATE, EXTENDED_BY_ID, EXTENSION_TYPE_ID, EXTENSION_NOTES)
+			values (GETDATE(), 1, @CONTACT_ID, @EXPIRY_DATE, @EXPIRY_DATE,
+			@EDITOR_ID, @EXTENSION_TYPE_ID, @EXTENSION_NOTES)
+			set @NEW_ROW_ID = @@IDENTITY
+			
+			--  update TB_EXPIRY_EDIT_LOG
+			update TB_EXPIRY_EDIT_LOG
+			set OLD_EXPIRY_DATE = @ORIGINAL_EXPIRY
+			where EXPIRY_EDIT_ID = @NEW_ROW_ID
+	
+			set @RESULT = 'Valid'		 
+		end
+	end
+	
+	
+	
+COMMIT TRANSACTION
+END TRY
+
+BEGIN CATCH
+IF (@@TRANCOUNT > 0)
+begin	
+ROLLBACK TRANSACTION
+set @RESULT = 'Invalid'
+end
+END CATCH
+
+RETURN
+
+SET NOCOUNT OFF
+
+
+
+GO
