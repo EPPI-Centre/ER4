ff5d55ce22a5481e81cc522206e850f7960f8083
Sergio Graziosi <s.graziosi@ucl.ac.uk>
2019-09-16 17:09:23 +0100

    Added even more "GO"s to end of each stored proc.

--- ERCentralAPI/SQL Changes Manager/SQLScripts/96.sql
+++ ERCentralAPI/SQL Changes Manager/SQLScripts/96.sql
@@ -34,6 +34,7 @@ DELETE FROM TB_ITEM_TIMEPOINT
 	WHERE ITEM_TIMEPOINT_ID = @ITEM_TIMEPOINT_ID
 
 SET NOCOUNT OFF
+GO
 
 /****** Object:  StoredProcedure [dbo].[st_ReviewWorkAllocationDelete]    Script Date: 16/09/2019 15:12:50 ******/
 SET ANSI_NULLS ON
@@ -58,7 +59,7 @@ if (@check != 1) return
 
 DELETE FROM TB_WORK_ALLOCATION
 WHERE WORK_ALLOCATION_ID = @WORK_ALLOCATION_ID
-
+GO
 
 /****** Object:  StoredProcedure [dbo].[st_SourceDelete]    Script Date: 16/09/2019 15:20:58 ******/
 SET ANSI_NULLS ON
