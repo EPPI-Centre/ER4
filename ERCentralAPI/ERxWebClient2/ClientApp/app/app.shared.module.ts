3efbba97ea3df562cf424589766017235fbbcbbe
qtnvpod <qtnvpod@ucl.ac.uk>
2020-04-03 16:25:14 +0100

    correction for last commit that had app.shared file incorrect. NB: Jeff.

--- ERCentralAPI/ERxWebClient2/ClientApp/app/app.shared.module.ts
+++ ERCentralAPI/ERxWebClient2/ClientApp/app/app.shared.module.ts
@@ -87,8 +87,8 @@ import { configurablereportComp } from './components/Reports/configurablereport.
 import { DuplicatesComponent } from './components/Duplicates/duplicates.component';
 import { codesetTree4Move } from './components/CodesetTrees/codesetTree4Move.component';
 import { BasicMAGComp } from './components/MAG/BasicMAGComp.component';
-import { AdvancedMAGFeaturesComponent } from './components/MAG/AdvancedMAGFeatures.component';
-import { MAGBrowser } from './components/MAG/MAGBrowser.component';
+//import { AdvancedMAGFeaturesComponent } from './components/MAG/AdvancedMAGFeatures.component';
+//import { MAGBrowser } from './components/MAG/MAGBrowser.component';
 import { MAGpaginatorComp } from './components/commonComponents/MAGpaginator.component';
 
 @NgModule({
@@ -158,8 +158,8 @@ import { MAGpaginatorComp } from './components/commonComponents/MAGpaginator.com
         configurablereportComp,
         DuplicatesComponent,
         codesetTree4Move,
-        AdvancedMAGFeaturesComponent,
-        MAGBrowser,
+        //AdvancedMAGFeaturesComponent,
+        //MAGBrowser,
         MAGpaginatorComp
 	],
     providers: [
@@ -199,9 +199,9 @@ import { MAGpaginatorComp } from './components/commonComponents/MAGpaginator.com
 			{ path: 'sources', component: SourcesComponent },
             { path: 'BuildModel', component: BuildModelComponent },
             { path: 'BasicMAGFeatures', component: BasicMAGComp },
-            { path: 'MAGBrowser', component: MAGBrowser },
+            //{ path: 'MAGBrowser', component: MAGBrowser },
             { path: 'ItemList', component: ItemListComp},
-            { path: 'AdvancedMAGFeatures', component: AdvancedMAGFeaturesComponent },
+            //{ path: 'AdvancedMAGFeatures', component: AdvancedMAGFeaturesComponent },
             { path: 'itemcodingOnly/:itemId', component: ItemCodingComp },
             { path: 'itemcoding/:itemId', component: ItemCodingFullComp },
             { path: 'EditItem/:itemId', component: editItemDetailsComp },
