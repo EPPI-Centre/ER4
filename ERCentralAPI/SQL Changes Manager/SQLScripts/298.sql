8d637725cc2c29a75d513e20afd7b7681e75c5e0
Sergio Graziosi <s.graziosi@ucl.ac.uk>
2020-12-02 17:09:24 +0000

    Bug QuickFix: Item List for included items going wrong when items are shared across reviews (fix is published).

--- ERCentralAPI/SQL Changes Manager/SQLScripts/298.sql
+++ ERCentralAPI/SQL Changes Manager/SQLScripts/298.sql
@@ -207,3 +207,223 @@ AS
       
 GO      
 
+USE [Reviewer]
+GO
+/****** Object:  StoredProcedure [dbo].[st_ItemList]    Script Date: 02/12/2020 16:22:28 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+ALTER procedure [dbo].[st_ItemList]
+(
+      @REVIEW_ID INT,
+      @SHOW_INCLUDED BIT = 'true',
+      @SHOW_DELETED BIT = 'false',
+      @SOURCE_ID INT = 0,
+      @ATTRIBUTE_SET_ID_LIST NVARCHAR(MAX) = '',
+      
+      @PageNum INT = 1,
+      @PerPage INT = 3,
+      @CurrentPage INT OUTPUT,
+      @TotalPages INT OUTPUT,
+      @TotalRows INT OUTPUT 
+)
+
+As
+
+SET NOCOUNT ON
+
+declare @RowsToRetrieve int
+Declare @ID table (ItemID bigint primary key )
+IF (@SOURCE_ID = 0) AND (@ATTRIBUTE_SET_ID_LIST = '') /* LIST ALL ITEMS IN THE REVIEW */
+BEGIN
+
+       --store IDs to build paged results as a simple join
+	  INSERT INTO @ID SELECT DISTINCT (I.ITEM_ID)
+      FROM TB_ITEM I
+      INNER JOIN TB_ITEM_REVIEW ON TB_ITEM_REVIEW.ITEM_ID = I.ITEM_ID AND 
+            TB_ITEM_REVIEW.REVIEW_ID = @REVIEW_ID
+            AND TB_ITEM_REVIEW.IS_INCLUDED = @SHOW_INCLUDED
+            AND TB_ITEM_REVIEW.IS_DELETED = @SHOW_DELETED
+
+	  SELECT @TotalRows = @@ROWCOUNT
+
+      set @TotalPages = @TotalRows/@PerPage
+
+      if @PageNum < 1
+      set @PageNum = 1
+
+      if @TotalRows % @PerPage != 0
+      set @TotalPages = @TotalPages + 1
+
+      set @RowsToRetrieve = @PerPage * @PageNum
+      set @CurrentPage = @PageNum;
+
+      WITH SearchResults AS
+      (
+      SELECT DISTINCT (ir.ITEM_ID), IS_DELETED, IS_INCLUDED, ir.MASTER_ITEM_ID,
+            ROW_NUMBER() OVER(order by SHORT_TITLE, ir.ITEM_ID) RowNum
+      FROM TB_ITEM i
+			INNER JOIN TB_ITEM_REVIEW ir on i.ITEM_ID = ir.ITEM_ID and ir.REVIEW_ID = @REVIEW_ID
+			INNER JOIN @ID id on id.ItemID = ir.ITEM_ID
+      )
+      Select SearchResults.ITEM_ID, II.[TYPE_ID], OLD_ITEM_ID, [dbo].fn_REBUILD_AUTHORS(II.ITEM_ID, 0) as AUTHORS,
+                  TITLE, PARENT_TITLE, SHORT_TITLE, DATE_CREATED, CREATED_BY, DATE_EDITED, EDITED_BY,
+                  [YEAR], [MONTH], STANDARD_NUMBER, CITY, COUNTRY, PUBLISHER, INSTITUTION, VOLUME, PAGES, EDITION, ISSUE, IS_LOCAL,
+                  AVAILABILITY, URL, ABSTRACT, COMMENTS, [TYPE_NAME], IS_DELETED, IS_INCLUDED, [dbo].fn_REBUILD_AUTHORS(II.ITEM_ID, 1) as PARENTAUTHORS
+                  , SearchResults.MASTER_ITEM_ID, DOI, KEYWORDS
+                  --, ROW_NUMBER() OVER(order by authors, TITLE) RowNum
+            FROM SearchResults
+                  INNER JOIN TB_ITEM II ON SearchResults.ITEM_ID = II.ITEM_ID
+                  INNER JOIN TB_ITEM_TYPE ON TB_ITEM_TYPE.[TYPE_ID] = II.[TYPE_ID]
+            WHERE RowNum > @RowsToRetrieve - @PerPage
+            AND RowNum <= @RowsToRetrieve
+            ORDER BY RowNum
+END
+ELSE /* FILTER BY A LIST OF ATTRIBUTES */
+
+IF (@ATTRIBUTE_SET_ID_LIST != '')
+BEGIN
+      SELECT @TotalRows = count(DISTINCT I.ITEM_ID)
+            FROM TB_ITEM_REVIEW I
+      INNER JOIN TB_ITEM_ATTRIBUTE ON TB_ITEM_ATTRIBUTE.ITEM_ID = I.ITEM_ID
+      INNER JOIN TB_ATTRIBUTE_SET ON TB_ATTRIBUTE_SET.ATTRIBUTE_ID = TB_ITEM_ATTRIBUTE.ATTRIBUTE_ID
+      INNER JOIN dbo.fn_Split_int(@ATTRIBUTE_SET_ID_LIST, ',') attribute_list ON attribute_list.value = TB_ATTRIBUTE_SET.ATTRIBUTE_SET_ID
+      INNER JOIN TB_ITEM_SET ON TB_ITEM_SET.ITEM_SET_ID = TB_ITEM_ATTRIBUTE.ITEM_SET_ID AND TB_ITEM_SET.IS_COMPLETED = 'TRUE'
+      INNER JOIN TB_REVIEW_SET ON TB_REVIEW_SET.SET_ID = TB_ITEM_SET.SET_ID AND TB_REVIEW_SET.REVIEW_ID = @REVIEW_ID -- Make sure the correct set is being used - the same code can appear in more than one set!
+
+      WHERE I.IS_INCLUDED = @SHOW_INCLUDED
+            AND I.IS_DELETED = @SHOW_DELETED
+            AND I.REVIEW_ID = @REVIEW_ID
+
+      set @TotalPages = @TotalRows/@PerPage
+
+      if @PageNum < 1
+      set @PageNum = 1
+
+      if @TotalRows % @PerPage != 0
+      set @TotalPages = @TotalPages + 1
+
+      set @RowsToRetrieve = @PerPage * @PageNum
+      set @CurrentPage = @PageNum;
+
+      WITH SearchResults AS
+      (
+      SELECT DISTINCT (I.ITEM_ID), IS_DELETED, IS_INCLUDED, TB_ITEM_REVIEW.MASTER_ITEM_ID, ADDITIONAL_TEXT,
+            ROW_NUMBER() OVER(order by SHORT_TITLE, I.ITEM_ID) RowNum
+      FROM TB_ITEM I
+       INNER JOIN TB_ITEM_REVIEW ON TB_ITEM_REVIEW.ITEM_ID = I.ITEM_ID AND 
+            TB_ITEM_REVIEW.REVIEW_ID = @REVIEW_ID
+            AND TB_ITEM_REVIEW.IS_INCLUDED = @SHOW_INCLUDED
+            AND TB_ITEM_REVIEW.IS_DELETED = @SHOW_DELETED
+
+      INNER JOIN TB_ITEM_ATTRIBUTE ON TB_ITEM_ATTRIBUTE.ITEM_ID = I.ITEM_ID INNER JOIN TB_ATTRIBUTE_SET ON TB_ATTRIBUTE_SET.ATTRIBUTE_ID = TB_ITEM_ATTRIBUTE.ATTRIBUTE_ID INNER JOIN dbo.fn_Split_int(@ATTRIBUTE_SET_ID_LIST, ',') attribute_list ON attribute_list.value = TB_ATTRIBUTE_SET.ATTRIBUTE_SET_ID INNER JOIN TB_ITEM_SET ON TB_ITEM_SET.ITEM_SET_ID = TB_ITEM_ATTRIBUTE.ITEM_SET_ID AND TB_ITEM_SET.IS_COMPLETED = 'TRUE'
+      INNER JOIN TB_REVIEW_SET ON TB_REVIEW_SET.SET_ID = TB_ITEM_SET.SET_ID AND TB_REVIEW_SET.REVIEW_ID = @REVIEW_ID -- Make sure the correct set is being used - the same code can appear in more than one set!
+      )
+      Select SearchResults.ITEM_ID, II.[TYPE_ID], OLD_ITEM_ID, [dbo].fn_REBUILD_AUTHORS(II.ITEM_ID, 0) as AUTHORS,
+                  TITLE, PARENT_TITLE, SHORT_TITLE, DATE_CREATED, CREATED_BY, DATE_EDITED, EDITED_BY,
+                  [YEAR], [MONTH], STANDARD_NUMBER, CITY, COUNTRY, PUBLISHER, INSTITUTION, VOLUME, PAGES, EDITION, ISSUE, IS_LOCAL,
+                  AVAILABILITY, URL, ABSTRACT, COMMENTS, [TYPE_NAME], IS_DELETED, IS_INCLUDED, [dbo].fn_REBUILD_AUTHORS(II.ITEM_ID, 1) as PARENTAUTHORS
+                  , SearchResults.MASTER_ITEM_ID, DOI, KEYWORDS, ADDITIONAL_TEXT
+                  --, ROW_NUMBER() OVER(order by authors, TITLE) RowNum
+            FROM SearchResults
+                  INNER JOIN TB_ITEM II ON SearchResults.ITEM_ID = II.ITEM_ID
+                  INNER JOIN TB_ITEM_TYPE ON TB_ITEM_TYPE.[TYPE_ID] = II.[TYPE_ID]
+            WHERE RowNum > @RowsToRetrieve - @PerPage
+            AND RowNum <= @RowsToRetrieve 
+            ORDER BY RowNum
+END
+ELSE -- LISTING SOURCELESS
+IF (@SOURCE_ID = -1)
+BEGIN
+       --store IDs to build paged results as a simple join
+	  INSERT INTO @ID SELECT DISTINCT IR.ITEM_ID
+		from TB_ITEM_REVIEW IR 
+      LEFT OUTER JOIN TB_ITEM_SOURCE TIS on IR.ITEM_ID = TIS.ITEM_ID
+      LEFT OUTER JOIN TB_SOURCE TS on TIS.SOURCE_ID = TS.SOURCE_ID and IR.REVIEW_ID = TS.REVIEW_ID
+      where IR.REVIEW_ID = @REVIEW_ID and TS.SOURCE_ID  is null
+
+	  SELECT @TotalRows = @@ROWCOUNT
+      set @TotalPages = @TotalRows/@PerPage
+
+      if @PageNum < 1
+      set @PageNum = 1
+
+      if @TotalRows % @PerPage != 0
+      set @TotalPages = @TotalPages + 1
+
+      set @RowsToRetrieve = @PerPage * @PageNum
+      set @CurrentPage = @PageNum;
+
+      WITH SearchResults AS
+      (
+      SELECT DISTINCT (I.ITEM_ID), IR.IS_DELETED, IS_INCLUDED, IR.MASTER_ITEM_ID,
+            ROW_NUMBER() OVER(order by SHORT_TITLE, I.ITEM_ID) RowNum
+      FROM TB_ITEM I
+      INNER JOIN TB_ITEM_TYPE ON TB_ITEM_TYPE.[TYPE_ID] = I.[TYPE_ID] 
+      INNER JOIN TB_ITEM_REVIEW IR ON IR.ITEM_ID = I.ITEM_ID AND IR.REVIEW_ID = @REVIEW_ID
+      INNER JOIN @ID id on id.ItemID = I.ITEM_ID
+      )
+      Select SearchResults.ITEM_ID, II.[TYPE_ID], OLD_ITEM_ID, [dbo].fn_REBUILD_AUTHORS(II.ITEM_ID, 0) as AUTHORS,
+                  TITLE, PARENT_TITLE, SHORT_TITLE, DATE_CREATED, CREATED_BY, DATE_EDITED, EDITED_BY,
+                  [YEAR], [MONTH], STANDARD_NUMBER, CITY, COUNTRY, PUBLISHER, INSTITUTION, VOLUME, PAGES, EDITION, ISSUE, IS_LOCAL,
+                  AVAILABILITY, URL, ABSTRACT, COMMENTS, [TYPE_NAME], IS_DELETED, IS_INCLUDED, [dbo].fn_REBUILD_AUTHORS(II.ITEM_ID, 1) as PARENTAUTHORS
+                  , SearchResults.MASTER_ITEM_ID, DOI, KEYWORDS
+                  --, ROW_NUMBER() OVER(order by authors, TITLE) RowNum
+            FROM SearchResults
+                  INNER JOIN TB_ITEM II ON SearchResults.ITEM_ID = II.ITEM_ID
+                  INNER JOIN TB_ITEM_TYPE ON TB_ITEM_TYPE.[TYPE_ID] = II.[TYPE_ID]
+            WHERE RowNum > @RowsToRetrieve - @PerPage
+            AND RowNum <= @RowsToRetrieve 
+            ORDER BY RowNum
+      
+END
+ELSE -- LISTING BY A SOURCE
+BEGIN
+      SELECT @TotalRows = count(I.ITEM_ID)
+      FROM TB_ITEM_REVIEW I
+      INNER JOIN TB_ITEM_SOURCE ON TB_ITEM_SOURCE.ITEM_ID = I.ITEM_ID AND TB_ITEM_SOURCE.SOURCE_ID = @SOURCE_ID
+      WHERE I.REVIEW_ID = @REVIEW_ID
+
+      set @TotalPages = @TotalRows/@PerPage
+
+      if @PageNum < 1
+      set @PageNum = 1
+
+      if @TotalRows % @PerPage != 0
+      set @TotalPages = @TotalPages + 1
+
+      set @RowsToRetrieve = @PerPage * @PageNum
+      set @CurrentPage = @PageNum;
+
+      WITH SearchResults AS
+      (
+      SELECT DISTINCT (I.ITEM_ID), IS_DELETED, IS_INCLUDED, TB_ITEM_REVIEW.MASTER_ITEM_ID,
+            ROW_NUMBER() OVER(order by SHORT_TITLE, I.ITEM_ID) RowNum
+      FROM TB_ITEM I
+      INNER JOIN TB_ITEM_TYPE ON TB_ITEM_TYPE.[TYPE_ID] = I.[TYPE_ID] INNER JOIN TB_ITEM_REVIEW ON TB_ITEM_REVIEW.ITEM_ID = I.ITEM_ID AND 
+            TB_ITEM_REVIEW.REVIEW_ID = @REVIEW_ID
+      INNER JOIN TB_ITEM_SOURCE ON TB_ITEM_SOURCE.ITEM_ID = I.ITEM_ID AND TB_ITEM_SOURCE.SOURCE_ID = @SOURCE_ID
+      )
+      Select SearchResults.ITEM_ID, II.[TYPE_ID], OLD_ITEM_ID, [dbo].fn_REBUILD_AUTHORS(II.ITEM_ID, 0) as AUTHORS,
+                  TITLE, PARENT_TITLE, SHORT_TITLE, DATE_CREATED, CREATED_BY, DATE_EDITED, EDITED_BY,
+                  [YEAR], [MONTH], STANDARD_NUMBER, CITY, COUNTRY, PUBLISHER, INSTITUTION, VOLUME, PAGES, EDITION, ISSUE, IS_LOCAL,
+                  AVAILABILITY, URL, ABSTRACT, COMMENTS, [TYPE_NAME], IS_DELETED, IS_INCLUDED, [dbo].fn_REBUILD_AUTHORS(II.ITEM_ID, 1) as PARENTAUTHORS
+                  , SearchResults.MASTER_ITEM_ID, DOI, KEYWORDS
+                  --, ROW_NUMBER() OVER(order by authors, TITLE) RowNum
+            FROM SearchResults
+                  INNER JOIN TB_ITEM II ON SearchResults.ITEM_ID = II.ITEM_ID
+                  INNER JOIN TB_ITEM_TYPE ON TB_ITEM_TYPE.[TYPE_ID] = II.[TYPE_ID]
+            WHERE RowNum > @RowsToRetrieve - @PerPage
+            AND RowNum <= @RowsToRetrieve
+            ORDER BY RowNum
+      
+END
+
+SELECT      @CurrentPage as N'@CurrentPage',
+            @TotalPages as N'@TotalPages',
+            @TotalRows as N'@TotalRows'
+
+
+SET NOCOUNT OFF
+GO
