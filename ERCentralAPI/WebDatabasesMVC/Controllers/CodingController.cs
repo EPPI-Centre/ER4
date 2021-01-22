d21d8d9406572561ded829297c0cf8445d204955
Sergio Graziosi <s.graziosi@ucl.ac.uk>
2021-01-22 19:19:36 +0000

    WebDbs: partial (and somewhat broken) "per item coding report".

---
+++ ERCentralAPI/WebDatabasesMVC/Controllers/CodingController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using BusinessLibrary.BusinessClasses;
+using Csla;
+using EPPIDataServices.Helpers;
+using ERxWebClient2.Controllers;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebDatabasesMVC;
+
+namespace WebDatabasesMVC.Controllers
+{
+    [Authorize]
+    public class CodingController : CSLAController
+    {
+        
+        public CodingController(ILogger<CodingController> logger) : base(logger)
+        {}
+
+        [HttpPost]
+        public IActionResult FetchItemCodingJSON([FromForm] long ItemId)
+        {
+            try
+            {
+                if (SetCSLAUser())
+                {
+                    int DBid = WebDbId;
+                    ItemCodingWithCodingTools res = new ItemCodingWithCodingTools();
+                    if (DBid > 0)
+                    {
+                        res.reviewSets  = DataPortal.Fetch<WebDbReviewSetsList>(new SingleCriteria<WebDbReviewSetsList, int>(DBid));
+                        WebDbItemSetListSelectionCriteria crit = new WebDbItemSetListSelectionCriteria(DBid, ItemId);
+                        res.itemSetList = DataPortal.Fetch<WebDbItemSetList>(crit);
+                    }
+                    return Json(res);
+                }
+                else return Unauthorized();
+            } 
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Error in ReviewSetList FetchJSON");
+                return StatusCode(500, e.Message);
+            }
+        }
+    }
+}
---
+++ ERCentralAPI/WebDatabasesMVC/ViewModels/ItemCodingWithCodingTools.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLibrary.BusinessClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebDatabasesMVC.Controllers;
+
+namespace BusinessLibrary.BusinessClasses
+{
+    public class ItemCodingWithCodingTools
+    {
+        public WebDbReviewSetsList reviewSets { get; set; }
+        public WebDbItemSetList itemSetList { get; set; }
+    }
+    
+}
