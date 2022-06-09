e7be9baad7153c4a1b8f906218ab516fde22e372
Sergio Graziosi <s.graziosi@ucl.ac.uk>
2022-06-09 14:19:49 +0100

    ER-Web: removed ERxWebClient2 older project.

--- ERCentralAPI/ERxWebClient2/wwwroot/lib/ui/src/apis/disableMeasurement.js
+++
@@ -1,8 +0,0 @@
-import { PRIORITY_ONE } from 'constants/actionPriority';
-import actions from 'actions';
-import core from 'core';
-
-export default store => () => {
-  store.dispatch(actions.disableElement('measurementToolGroupButton', PRIORITY_ONE));
-  core.setToolMode('AnnotationEdit');
-};
