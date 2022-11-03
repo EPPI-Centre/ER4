a5c63362ca257cdfe17e668cd990275e3d66f92e
qtnvpod <qtnvpod@ucl.ac.uk>
2022-11-03 13:24:00 +0000

    Adding parent class for generic fields, not completed.

--- ERCentralAPI/ER-Web/Zotero/ZoteroConferenceProceeding.cs
+++ ERCentralAPI/ER-Web/Zotero/ZoteroConferenceProceeding.cs
@@ -1,11 +1,12 @@
 ﻿using BusinessLibrary.BusinessClasses;
 using Csla;
+using ER_Web.Zotero;
 using ERxWebClient2.Controllers;
 using Newtonsoft.Json;
 
 namespace ERxWebClient2.Zotero
 {
-    public class ZoteroConferenceProceeding : IMapZoteroReference
+    public class ZoteroConferenceProceeding : ZoteroCreator, IMapZoteroReference
     {
         private CollectionType _conferenceProceeding;
         public ZoteroConferenceProceeding(CollectionType conferenceProceeding)
@@ -17,54 +18,13 @@ namespace ERxWebClient2.Zotero
         {
             try
             {
-                var smartDate = new SmartDate();
-                var parseDateResult = SmartDate.TryParse(_conferenceProceeding.date, ref smartDate);
-                if (!parseDateResult) throw new System.Exception("Date parsing exception");
-                var smartDateAdded = new SmartDate();
-                var parseDateAddedResult = SmartDate.TryParse(_conferenceProceeding.dateAdded, ref smartDateAdded);
-                if (!parseDateAddedResult) throw new System.Exception("Date parsing exception");
-                var smartDateModified = new SmartDate();
-                var parseDateModifiedResult = SmartDate.TryParse(_conferenceProceeding.dateModified, ref smartDateModified);
-                if (!parseDateModifiedResult) throw new System.Exception("Date parsing exception");
-
-
-                newERWebItem.Title = _conferenceProceeding.title;
-                newERWebItem.TypeId = 5;
-                newERWebItem.TypeName = "Conference Proceedings";
-                newERWebItem.ShortTitle = _conferenceProceeding.shortTitle;
-                newERWebItem.ParentTitle = _conferenceProceeding.parentTitle;
-                newERWebItem.DateCreated = smartDateAdded;
-                newERWebItem.CreatedBy = _conferenceProceeding.createdBy;
-                newERWebItem.DateEdited = smartDateModified;
-                newERWebItem.EditedBy = _conferenceProceeding.editedBy;
-                newERWebItem.Year = smartDate.Date.Year.ToString(); 
-                newERWebItem.Month = smartDate.Date.Month.ToString();
-                newERWebItem.StandardNumber = _conferenceProceeding.ISSN;
-                newERWebItem.City = _conferenceProceeding.place;
-                newERWebItem.Country = _conferenceProceeding.place;//TODO COuntry and city fix
-                newERWebItem.Publisher = _conferenceProceeding.publisher;
-                newERWebItem.Institution = _conferenceProceeding.institution;
-                newERWebItem.Volume = _conferenceProceeding.volume;
-                newERWebItem.Pages = _conferenceProceeding.pages;
-                newERWebItem.Edition = _conferenceProceeding.edition;
-                newERWebItem.Issue = _conferenceProceeding.issue;
-                newERWebItem.IsLocal = false; //TODO what is this
-                newERWebItem.Availability = ""; //TODO what is this,
-                newERWebItem.URL = _conferenceProceeding.url;
-                newERWebItem.MasterItemId = 0;//TODO what is this,
-                newERWebItem.Abstract = _conferenceProceeding.abstractNote;
-                newERWebItem.Comments = _conferenceProceeding.comments;
-                newERWebItem.DOI = _conferenceProceeding.DOI;
-                newERWebItem.Keywords = "";
-                string consolidatedAuthors = "";
-                foreach (var creator in _conferenceProceeding.creators)
-                {
-                    consolidatedAuthors += creator.firstName + " " + creator.lastName;
-                }
-                newERWebItem.Authors = consolidatedAuthors;
-                var erWebItem = new ERWebItem();
-                erWebItem.Item = newERWebItem;
+                var erWebItem = CreateErWebItemFromCollection(newERWebItem, _conferenceProceeding);
+                erWebItem.Item.TypeId = 5;
+                erWebItem.Item.TypeName = "Conference Proceedings";
+                erWebItem.Item.StandardNumber = _conferenceProceeding.ISBN;
                 return erWebItem;
+               
+                
 
             }
             catch (System.Exception ex)
---
+++ ERCentralAPI/ER-Web/Zotero/ZoteroCreator.cs
@@ -0,0 +1,51 @@
+﻿using BusinessLibrary.BusinessClasses;
+using Csla;
+using ERxWebClient2.Controllers;
+using ERxWebClient2.Zotero;
+
+namespace ER_Web.Zotero
+{
+    public abstract class ZoteroCreator
+    {
+        public ERWebItem CreateErWebItemFromCollection(Item newERWebItem, CollectionType collectionType) { 
+            
+            var smartDate = new SmartDate();
+            var parseDateResult = SmartDate.TryParse(collectionType.date, ref smartDate);
+            if (!parseDateResult) throw new System.Exception("Date parsing exception");
+            var smartDateAdded = new SmartDate();
+            var parseDateAddedResult = SmartDate.TryParse(collectionType.dateAdded, ref smartDateAdded);
+            if (!parseDateAddedResult) throw new System.Exception("Date parsing exception");
+            var smartDateModified = new SmartDate();
+            var parseDateModifiedResult = SmartDate.TryParse(collectionType.dateModified, ref smartDateModified);
+            if (!parseDateModifiedResult) throw new System.Exception("Date parsing exception");
+
+            newERWebItem.Title = collectionType.title;       
+            string consolidatedAuthors = "";
+            foreach (var creator in collectionType.creators)
+            {
+                consolidatedAuthors += creator.firstName + " " + creator.lastName;
+            }
+            newERWebItem.Authors = consolidatedAuthors;
+            newERWebItem.Abstract = collectionType.abstractNote;
+            newERWebItem.DateCreated = smartDateAdded;
+            newERWebItem.DateEdited = smartDateModified;
+            newERWebItem.Edition = collectionType.edition;
+            newERWebItem.Institution = collectionType.place;
+            newERWebItem.Pages = collectionType.numPages;
+            newERWebItem.Publisher = collectionType.publisher;
+            newERWebItem.ShortTitle = collectionType.shortTitle;
+            newERWebItem.Volume = collectionType.volume;
+            newERWebItem.IsLocal = false;
+            newERWebItem.Pages = collectionType.pages;
+            newERWebItem.Issue = collectionType.issue;
+            newERWebItem.City = collectionType.archiveLocation;
+            newERWebItem.ParentTitle = collectionType.parentTitle;
+            newERWebItem.ParentAuthors = collectionType.parentAuthors;
+            newERWebItem.DOI = collectionType.DOI;
+
+            var erWebItem = new ERWebItem();
+            erWebItem.Item = newERWebItem;
+            return erWebItem;
+        }
+    }
+}
