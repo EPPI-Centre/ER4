0f406d2408968da81e8664596c1ca417bdc2d004
jbrunton27 <j.brunton@ucl.ac.uk>
2023-04-26 09:33:49 -0400

    Integration tests still under development

---
+++ ERCentralAPI/IntegrationTests/By Controller Tests/CodingTests.cs
@@ -0,0 +1,194 @@
+﻿using BusinessLibrary.BusinessClasses;
+using CsvHelper.TypeConversion;
+using ERxWebClient2.Controllers;
+using FluentAssertions;
+using FluentAssertions.Primitives;
+using IntegrationTests.Fixtures;
+using IntegrationTests.Utils;
+using Microsoft.Azure.DataLake.Store;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Composition;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+
+
+
+namespace IntegrationTests.By_Controller_Tests
+{
+
+
+    [Collection("Database collection")]
+    public class CodingTests : FixedLoginTest
+    {
+        //await InnerLoginToReview("bob", "123", 12);
+        protected override string username { get; set; }
+        protected override string password { get; set; }
+        protected override int ReviewId { get; set; }
+
+        public CodingTests(ApiWebApplicationFactory fixture) : base(fixture)
+        {
+            username = "bob";
+            password = "123";
+            ReviewId = 12;
+        }
+
+        /*
+        [Theory]
+        [InlineData("20 refs.txt")]
+        public async Task CodingAssignCodeToItem(string filename, string importFilterName = "RIS")
+        {
+            //every [Fact] should start with this line
+            (await AuthenticationDone()).Should().Be(true);
+
+            //upload the file, check the answer
+            string fileCont = File.ReadAllText(@"Files\" + filename);
+            _ = fileCont.Should().NotBeNullOrEmpty();
+            UploadOrCheckSource data = new UploadOrCheckSource() { fileContent = fileCont, importFilter = importFilterName, source_Name = filename };
+            IncomingItemsListJSON? resS = await client.PostAndDeserialize<IncomingItemsListJSON>("api/Sources/UploadSource", data);
+            resS.Should().NotBeNull();
+
+            resS.totalReferences.Should().Be(20);
+            resS.incomingItems.Count.Should().Be(0);
+            //var itemID = resS.
+
+            // create screening tool
+
+            ReviewSetUpdateCommandJSON rsc = new ReviewSetUpdateCommandJSON();
+            // settings for a new screening tool
+            rsc.SetId = -1;
+            rsc.ReviewSetId = -1;
+            rsc.SetName = "test 1";
+            rsc.setOrder = 0;
+            rsc.setDescription = "";
+            rsc.CodingIsFinal = true;
+            rsc.AllowCodingEdits = true;
+            rsc.SetTypeId = 5; // screening tool
+            rsc.usersCanEditURLs = false;
+
+            JsonNode? resCT = await client.PostAndDeserialize("api/Codeset/ReviewSetCreate", rsc);
+            resCT.Should().NotBeNull();
+
+            JsonNode? CSRes = await client.GetAndDeserialize("api/Codeset/CodesetsByReview");
+            CSRes.AsArray().Count().Should().Be(1);
+            var setID = (int)CSRes[0]["setId"];
+            var reviewSetID = (int)CSRes[0]["reviewSetId"];
+
+            // add a couple of codes to the tool
+            AttributeSetCreateOrUpdateJSON incAttr = new AttributeSetCreateOrUpdateJSON();
+            // settings for a new 'Include' code
+            incAttr.setId = setID;
+            incAttr.parentAttributeId = 0;
+            incAttr.attributeTypeId = 10;
+            incAttr.attributeOrder = 0;
+            incAttr.attributeName = "Include 1";
+            incAttr.attributeSetDescription = "";
+            incAttr.contactId = 5;  // this is the default login person
+            incAttr.originalAttributeID = 0;
+            incAttr.attributeSetId = 0;
+            incAttr.attributeId = 0;
+            incAttr.extURL = "";
+            incAttr.extType = "";
+
+            AttributeSetCreateOrUpdateJSON excAttr = new AttributeSetCreateOrUpdateJSON();
+            // settings for a new 'Exclude' code
+            excAttr.setId = setID;
+            excAttr.parentAttributeId = 0;
+            excAttr.attributeTypeId = 11;
+            excAttr.attributeOrder = 1;
+            excAttr.attributeName = "Exclude 1";
+            excAttr.attributeSetDescription = "";
+            excAttr.contactId = 5;  // this is the default login person
+            excAttr.originalAttributeID = 0;
+            excAttr.attributeSetId = 0;
+            excAttr.attributeId = 0;
+            excAttr.extURL = "";
+            excAttr.extType = "";
+
+            JsonNode? resCI = await client.PostAndDeserialize("api/Codeset/AttributeCreate", incAttr);
+            resCI.Should().NotBeNull();
+
+            JsonNode? resCE = await client.PostAndDeserialize("api/Codeset/AttributeCreate", excAttr);
+            resCE.Should().NotBeNull();
+
+            JsonNode? res = await client.GetAndDeserialize("api/Codeset/CodesetsByReview");
+            var attributeName = res[0]["attributes"]["attributesList"][1]["attributeName"].ToString();
+            var attributeId = res[0]["attributes"]["attributesList"][1]["attributeId"].ToString();
+
+            ReviewInfo? Res = await GetReviewInfo();
+            MVCReviewInfo def = new MVCReviewInfo();
+            def.bL_AUTH_CODE = Res.BL_AUTH_CODE;
+            def.bL_ACCOUNT_CODE = Res.BL_ACCOUNT_CODE;
+            def.bL_CC_AUTH_CODE = Res.BL_CC_AUTH_CODE;
+            def.bL_CC_TX = Res.BL_CC_TX;
+            def.bL_TX = Res.BL_TX;
+            def.reviewId = Res.ReviewId;
+            def.reviewName = Res.ReviewName;
+            def.screeningAutoExclude = Res.ScreeningAutoExclude;
+            def.screeningCodeSetId = Res.ScreeningCodeSetId;
+            def.screeningIndexed = Res.ScreeningIndexed;
+            def.screeningListIsGood = Res.ScreeningListIsGood;
+            def.screeningMode = Res.ScreeningMode;
+            def.screeningMode = Res.ScreeningMode;
+            def.screeningModelRunning = Res.ScreeningModelRunning;
+            def.screeningNPeople = Res.ScreeningNPeople;
+            def.screeningReconcilliation = Res.ScreeningReconcilliation;
+
+            MVCItemAttributeSaveCommand abc = new MVCItemAttributeSaveCommand();
+            abc.additionalText = "";
+            abc.attributeId = int.Parse(attributeId);
+            abc.itemArmId = 0;
+            abc.itemAttributeId = 0;
+            abc.itemId = 3; // hardcoded itemId
+            abc.itemSetId = 0;
+            abc.saveType = "Insert";
+            abc.setId = setID;
+            abc.revInfo = def;
+
+            // assign code to item
+            JsonNode? ghi = await AddItemAttribute(abc);
+            var itemAttributeId = ghi["itemAttributeId"].ToString();
+            var itemSetId = ghi["itemSetId"].ToString();
+            
+
+            // update coding
+            abc.additionalText = "some new interesting text...";
+            abc.itemAttributeId = int.Parse(itemAttributeId);
+            abc.itemSetId = int.Parse(itemSetId);
+            abc.saveType = "Update";
+            JsonNode? jkl = await UpdateItemAttribute(abc);
+
+
+        }
+        */
+
+
+    }
+}
+
+namespace IntegrationTests.Fixtures
+{
+    public abstract partial class IntegrationTest : IClassFixture<ApiWebApplicationFactory>
+    {
+        
+        /*
+        public async Task<JsonNode?> AddItemAttribute(MVCItemAttributeSaveCommand rsc)
+        {
+            JsonNode? res = await client.PostAndDeserialize("api/ItemSetList/ExcecuteItemAttributeSaveCommand", rsc);
+            res.Should().NotBeNull();
+            return res;
+        }
+
+        public async Task<JsonNode?> UpdateItemAttribute(MVCItemAttributeSaveCommand rsc)
+        {
+            JsonNode? res = await client.PostAndDeserialize("api/ItemSetList/ExcecuteItemAttributeSaveCommand", rsc);
+            res.Should().NotBeNull();
+            return res;
+        }
+        */
+    }
+}
---
+++ ERCentralAPI/IntegrationTests/Story Tests/BuildDemoReviewStory.cs
@@ -0,0 +1,168 @@
+﻿using BusinessLibrary.BusinessClasses;
+using ERxWebClient2.Controllers;
+using FluentAssertions;
+using IntegrationTests.Fixtures;
+using IntegrationTests.Utils;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace IntegrationTests.Story_Tests
+{
+    /// <summary>
+    /// 1. Create review, upload 20 Items
+    /// 2. Create screening tool
+    /// 3. Code 20 items
+    /// 4. Inspect result using report? frequency? not sure yet
+    /// 
+
+    /// </summary>
+    [Collection("Database collection")]
+    public class BuildDemoReviewStory : FixedLoginTest
+    {
+        protected override string username { get; set; }
+        protected override string password { get; set; }
+        protected override int ReviewId { get; set; }
+        public BuildDemoReviewStory(ApiWebApplicationFactory fixture) : base(fixture)
+        {
+            username = "bob";
+            password = "123";
+            ReviewId = 12;
+        }
+
+        [Theory]
+        [InlineData("20 refs.txt")]
+        public async Task CodingAssignCodeToItem(string filename, string importFilterName = "RIS")
+        {
+            //every [Fact] should start with this line
+            (await AuthenticationDone()).Should().Be(true);
+
+            //upload the file, check the answer
+            string fileCont = File.ReadAllText(@"Files\" + filename);
+            _ = fileCont.Should().NotBeNullOrEmpty();
+            UploadOrCheckSource data = new UploadOrCheckSource() { fileContent = fileCont, importFilter = importFilterName, source_Name = filename };
+            IncomingItemsListJSON? resS = await client.PostAndDeserialize<IncomingItemsListJSON>("api/Sources/UploadSource", data);
+            resS.Should().NotBeNull();
+            resS.totalReferences.Should().Be(20);
+
+            // create screening tool
+            var codesetName = "Screen on T&A";
+            var setTypeID = 5; // screening tool
+            var allowCodingEdits = true; // not locked
+            var codingIsFinal = true; // single coding
+            JsonNode? CSRes = await CreateCodeset(codesetName, setTypeID, allowCodingEdits, codingIsFinal);
+
+            // get the new setId and reviewSetId
+            CSRes = await GetCodesets();
+            var setId = (int)CSRes[0]["setId"];
+            var reviewSetId = (int)CSRes[0]["reviewSetId"];
+
+            // add an exclude code
+            var attributeName = "Exclude 1";
+            var attributeTypeId = 11; // exclude code
+            JsonNode? AttrRes1 = await AddCode(setId, reviewSetId, attributeName, attributeTypeId);
+
+            // add an exclude code
+            attributeName = "Exclude 2";
+            attributeTypeId = 11; // exclude code
+            JsonNode? AttrRes2 = await AddCode(setId, reviewSetId, attributeName, attributeTypeId);
+
+            // add an exclude code
+            attributeName = "Exclude 3";
+            attributeTypeId = 11; // exclude code
+            JsonNode? AttrRes3 = await AddCode(setId, reviewSetId, attributeName, attributeTypeId);
+
+            // add an include code
+            attributeName = "Include 1";
+            attributeTypeId = 10; // include code
+            JsonNode? AttrRes4 = await AddCode(setId, reviewSetId, attributeName, attributeTypeId);
+
+            CSRes = await GetCodesets();
+            var attributeIdExc1 = int.Parse(CSRes[0]["attributes"]["attributesList"][0]["attributeId"].ToString());
+            var attributeIdExc2 = int.Parse(CSRes[0]["attributes"]["attributesList"][1]["attributeId"].ToString());
+            var attributeIdExc3 = int.Parse(CSRes[0]["attributes"]["attributesList"][2]["attributeId"].ToString());
+            var attributeIdInc1 = int.Parse(CSRes[0]["attributes"]["attributesList"][0]["attributeId"].ToString());
+
+
+            ReviewInfo? Res = await GetReviewInfo();
+
+            // assign screening codes to 5 items
+            var itemId = 1;
+            var additionalText = "";
+            JsonNode? item1 = await AddItemAttribute(Res, setId, attributeIdExc1, itemId, additionalText);
+            itemId = 2;
+            additionalText = "";
+            JsonNode? item2 = await AddItemAttribute(Res, setId, attributeIdExc2, itemId, additionalText);
+            itemId = 3;
+            additionalText = "";
+            JsonNode? item3 = await AddItemAttribute(Res, setId, attributeIdExc3, itemId, additionalText);
+            itemId = 4;
+            additionalText = "not sure about this one";
+            JsonNode? item4 = await AddItemAttribute(Res, setId, attributeIdInc1, itemId, additionalText);
+            itemId = 5;
+            additionalText = "";
+            JsonNode? item5 = await AddItemAttribute(Res, setId, attributeIdExc1, itemId, additionalText);
+
+
+            // get items with this code...
+
+
+
+            //var itemAttributeId = ghi["itemAttributeId"].ToString();
+            //var itemSetId = ghi["itemSetId"].ToString();
+
+
+        }
+
+
+    }
+}
+
+namespace IntegrationTests.Fixtures
+{
+    public abstract partial class IntegrationTest : IClassFixture<ApiWebApplicationFactory>
+    {
+        public async Task<JsonNode?> AddItemAttribute(ReviewInfo Res, int setId, int attributeId, int itemId, string additionalText)
+        {
+            MVCReviewInfo def = new MVCReviewInfo();
+            def.bL_AUTH_CODE = Res.BL_AUTH_CODE;
+            def.bL_ACCOUNT_CODE = Res.BL_ACCOUNT_CODE;
+            def.bL_CC_AUTH_CODE = Res.BL_CC_AUTH_CODE;
+            def.bL_CC_TX = Res.BL_CC_TX;
+            def.bL_TX = Res.BL_TX;
+            def.reviewId = Res.ReviewId;
+            def.reviewName = Res.ReviewName;
+            def.screeningAutoExclude = Res.ScreeningAutoExclude;
+            def.screeningCodeSetId = Res.ScreeningCodeSetId;
+            def.screeningIndexed = Res.ScreeningIndexed;
+            def.screeningListIsGood = Res.ScreeningListIsGood;
+            def.screeningMode = Res.ScreeningMode;
+            def.screeningMode = Res.ScreeningMode;
+            def.screeningModelRunning = Res.ScreeningModelRunning;
+            def.screeningNPeople = Res.ScreeningNPeople;
+            def.screeningReconcilliation = Res.ScreeningReconcilliation;
+
+            MVCItemAttributeSaveCommand abc = new MVCItemAttributeSaveCommand();
+            abc.additionalText = additionalText;
+            abc.attributeId = attributeId;
+            abc.itemArmId = 0;
+            abc.itemAttributeId = 0;
+            abc.itemId = itemId; 
+            abc.itemSetId = 0;
+            abc.saveType = "Insert";
+            abc.setId = setId;
+            abc.revInfo = def;
+
+            JsonNode? res = await client.PostAndDeserialize("api/ItemSetList/ExcecuteItemAttributeSaveCommand", abc);
+            res.Should().NotBeNull();
+            return res;
+        }
+    }
+}
+    
