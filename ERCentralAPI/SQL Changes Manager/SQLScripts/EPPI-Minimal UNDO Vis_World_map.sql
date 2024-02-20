721aff14282b73a78f9b61a09c706c179559c42f
sgraziosi <s.graziosi@ucl.ac.uk>
2024-02-20 17:28:15 +0000

    EPPI Vis added an ad-hoc (helper) script to help creating "countries" data

--- ERCentralAPI/SQL Changes Manager/SQLScripts/EPPI-Minimal UNDO Vis_World_map.sql
+++ ERCentralAPI/SQL Changes Manager/SQLScripts/EPPI-Minimal UNDO Vis_World_map.sql
@@ -2,43 +2,5 @@
 --to ensure that EPPI Vis and ER can run without errors when one leaves the EPPI-Vis_World_map branch, after having (manually) applied the SQL changes in the dedicated file
 USE [Reviewer]
 GO
-/****** Object:  StoredProcedure [dbo].[st_WebDBget]    Script Date: 06/02/2024 14:02:37 ******/
-SET ANSI_NULLS OFF
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-
-CREATE or ALTER       procedure [dbo].[st_WebDBget]
-(
-	@WebDBid int,
-	@RevId int	
-)
-
-As
-BEGIN
-select [WEBDB_ID]
-		  ,[REVIEW_ID]
-		  ,[WITH_ATTRIBUTE_ID]
-		  ,[IS_OPEN]
-		  ,w.[USERNAME]
-		  ,[WEBDB_NAME]
-		  ,SUBTITLE
-		  ,w.[DESCRIPTION]
-		  ,c1.CONTACT_NAME as [CREATED_BY]
-		  ,c2.CONTACT_NAME as [EDITED_BY]
-		  ,w.[MAP_TITLE]
-		  ,w.[MAP_URL]
-		  ,w.[HEADER_IMAGE_1_URL]
-		  ,w.[HEADER_IMAGE_2_URL]
-		  ,w.[HEADER_IMAGE_3_URL]
-		  ,w.HEADER_IMAGE_1
-		  ,w.HEADER_IMAGE_2
-		  ,w.HEADER_IMAGE_3
-		  --,[WORLD_MAP]
-	  FROM [TB_WEBDB] w
-	  inner join TB_CONTACT c1 on w.CREATED_BY = c1.CONTACT_ID
-	  inner join TB_CONTACT c2 on w.EDITED_BY = c2.CONTACT_ID 
-	  where w.WEBDB_ID = @WebDBid AND w.REVIEW_ID = @RevId
-END
-GO
+--put whatever needs to be revertes here, so to ensure EPPI Vis and ER4-6 can work correctly when leaving the VIS-specicif branch...
 
