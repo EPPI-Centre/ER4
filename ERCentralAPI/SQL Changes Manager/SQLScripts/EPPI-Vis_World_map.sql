5ed5f098a45bbeb539eb031446bc4d2542d01ff2
sgraziosi <s.graziosi@ucl.ac.uk>
2024-03-14 17:06:41 +0000

    ER / VIS other side of an "attempting to sort out SQL changes", done on the World-Map feature branch.

--- ERCentralAPI/SQL Changes Manager/SQLScripts/EPPI-Vis_World_map.sql
+++ ERCentralAPI/SQL Changes Manager/SQLScripts/EPPI-Vis_World_map.sql
@@ -548,13 +548,13 @@ GO
 
 USE [Reviewer]
 GO
-/****** Object:  StoredProcedure [dbo].[st_WebDBget]    Script Date: 06/02/2024 14:02:37 ******/
+/****** Object:  StoredProcedure [dbo].[st_WebDBget]    Script Date: 01/03/2024 11:49:52 ******/
 SET ANSI_NULLS OFF
 GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE or ALTER       procedure [dbo].[st_WebDBget]
+ALTER       procedure [dbo].[st_WebDBget]
 (
 	@WebDBid int,
 	@RevId int	
@@ -562,13 +562,31 @@ CREATE or ALTER       procedure [dbo].[st_WebDBget]
 
 As
 BEGIN
-select w.*
+select [WEBDB_ID]
+		  ,[REVIEW_ID]
+		  ,[WITH_ATTRIBUTE_ID]
+		  ,[IS_OPEN]
+		  ,w.[USERNAME]
+		  ,[WEBDB_NAME]
+		  ,SUBTITLE
+		  ,w.[DESCRIPTION]
 		  ,c1.CONTACT_NAME as [CREATED_BY]
 		  ,c2.CONTACT_NAME as [EDITED_BY]
+		  ,w.[MAP_TITLE]
+		  ,w.[MAP_URL]
+		  ,w.[HEADER_IMAGE_1_URL]
+		  ,w.[HEADER_IMAGE_2_URL]
+		  ,w.[HEADER_IMAGE_3_URL]
+		  ,w.HEADER_IMAGE_1
+		  ,w.HEADER_IMAGE_2
+		  ,w.HEADER_IMAGE_3
+		  ,[WORLD_MAP]
+		  ,[HIDDEN_FIELDS]
 	  FROM [TB_WEBDB] w
 	  inner join TB_CONTACT c1 on w.CREATED_BY = c1.CONTACT_ID
 	  inner join TB_CONTACT c2 on w.EDITED_BY = c2.CONTACT_ID 
 	  where w.WEBDB_ID = @WebDBid AND w.REVIEW_ID = @RevId
 END
+
 GO
 
