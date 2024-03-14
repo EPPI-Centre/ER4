ea1cbc5169aeff3aea5d1991507865168aec8139
sgraziosi <s.graziosi@ucl.ac.uk>
2024-03-14 10:29:43 +0000

    ER and Vis - safety change to 'EPPI-Vis_World_map.sql' script, which will become a numbered changes script when merging this branch.

--- ERCentralAPI/SQL Changes Manager/SQLScripts/EPPI-Vis_World_map.sql
+++ ERCentralAPI/SQL Changes Manager/SQLScripts/EPPI-Vis_World_map.sql
@@ -562,25 +562,9 @@ CREATE or ALTER       procedure [dbo].[st_WebDBget]
 
 As
 BEGIN
-select [WEBDB_ID]
-		  ,[REVIEW_ID]
-		  ,[WITH_ATTRIBUTE_ID]
-		  ,[IS_OPEN]
-		  ,w.[USERNAME]
-		  ,[WEBDB_NAME]
-		  ,SUBTITLE
-		  ,w.[DESCRIPTION]
+select w.*
 		  ,c1.CONTACT_NAME as [CREATED_BY]
 		  ,c2.CONTACT_NAME as [EDITED_BY]
-		  ,w.[MAP_TITLE]
-		  ,w.[MAP_URL]
-		  ,w.[HEADER_IMAGE_1_URL]
-		  ,w.[HEADER_IMAGE_2_URL]
-		  ,w.[HEADER_IMAGE_3_URL]
-		  ,w.HEADER_IMAGE_1
-		  ,w.HEADER_IMAGE_2
-		  ,w.HEADER_IMAGE_3
-		  ,[WORLD_MAP]
 	  FROM [TB_WEBDB] w
 	  inner join TB_CONTACT c1 on w.CREATED_BY = c1.CONTACT_ID
 	  inner join TB_CONTACT c2 on w.EDITED_BY = c2.CONTACT_ID 
