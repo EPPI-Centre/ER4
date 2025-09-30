c51426504324d56095eb1b6be08a1f37b3988d23
Sergio Graziosi <s.graziosi@ucl.ac.uk>
2025-09-30 08:37:23 +0100

    ER4: V.4.17.0.0

--- EPPIReviewer4.2015/Server/Properties/AssemblyInfo.cs
+++ EPPIReviewer4.2015/Server/Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("4.16.3.0")]
+[assembly: AssemblyVersion("4.17.0.0")]
 
 [assembly: AssemblyFileVersion("1.0.0.0")]
