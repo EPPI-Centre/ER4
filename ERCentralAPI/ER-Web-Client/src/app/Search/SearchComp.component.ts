963e070f103a8d0e2e59ac0a6ccc372b37cca5b8
JeffBrunton <53277974+JeffBrunton@users.noreply.github.com>
2026-01-16 11:39:49 -0500

    ER6 - added classifier descriptions to the pre-built classifiers

--- ERCentralAPI/ER-Web-Client/src/app/Search/SearchComp.component.ts
+++ ERCentralAPI/ER-Web-Client/src/app/Search/SearchComp.component.ts
@@ -95,6 +95,14 @@ export class SearchComp implements OnInit, OnDestroy {
   public modelResultsSection: boolean = false;
   public modelResultsAllReviewSection: boolean = false;
   public radioButtonApplyModelSection: boolean = false;
+  public prebuiltClassifier1Selected: boolean = false;
+  public prebuiltClassifier2Selected: boolean = false;
+  public prebuiltClassifier3Selected: boolean = false;
+  public prebuiltClassifier4Selected: boolean = false;
+  public prebuiltClassifier5Selected: boolean = false;
+  public prebuiltClassifier6Selected: boolean = false;
+  public prebuiltClassifier9Selected: boolean = false;
+  public prebuiltClassifierDescription: string = "";
   public isCollapsed: boolean = false;
   public isCollapsedVisualise: boolean = false;
   public firstName: string = '';
@@ -331,6 +339,13 @@ export class SearchComp implements OnInit, OnDestroy {
     this.ModelSection = false;
     this.modelResultsSection = false;
     this.radioButtonApplyModelSection = false;
+    this.prebuiltClassifier1Selected = false;
+    this.prebuiltClassifier2Selected = false;
+    this.prebuiltClassifier3Selected = false;
+    this.prebuiltClassifier4Selected = false;
+    this.prebuiltClassifier5Selected = false;
+    this.prebuiltClassifier6Selected = false;
+    this.prebuiltClassifier9Selected = false;
     this.ShowVisualiseSection = false;
     this.CheckScreeningSection = false;
     //this.PriorityScreeningSection = false;
@@ -437,9 +452,17 @@ export class SearchComp implements OnInit, OnDestroy {
 
   CustomModels(modelNum: number) {
     if (this.modelTitle == '') {
-
       this.ModelSelected = false;
     }
+
+    this.prebuiltClassifier1Selected = false;
+    this.prebuiltClassifier2Selected = false;
+    this.prebuiltClassifier3Selected = false;
+    this.prebuiltClassifier4Selected = false;
+    this.prebuiltClassifier5Selected = false;
+    this.prebuiltClassifier6Selected = false;
+    this.prebuiltClassifier9Selected = false;
+
     if (modelNum == 7) {
       this.modelNum = 7;
       this.modelResultsSection = !this.modelResultsSection;
@@ -450,20 +473,49 @@ export class SearchComp implements OnInit, OnDestroy {
       this.modelResultsAllReviewSection = !this.modelResultsAllReviewSection;
       this.modelResultsSection = false;
       this.ModelSelected = false;
+
     }
 
   }
   SetModelSelection(num: number) {
-
-    //alert('SelectedNode is: ' + this._reviewSetsService.selectedNode);
     this.modelNum = num;
     this.NewSearchSection = false;
     this.modelResultsSection = false;
     this.CheckScreeningSection = false;
-    //this.PriorityScreeningSection = false;
-    //alert('Model Number is: ' + this.modelNum);
+    this.prebuiltClassifier1Selected = false;
+    this.prebuiltClassifier2Selected = false;
+    this.prebuiltClassifier3Selected = false;
+    this.prebuiltClassifier4Selected = false;
+    this.prebuiltClassifier5Selected = false;
+    this.prebuiltClassifier6Selected = false;
+    this.prebuiltClassifier9Selected = false;
 
+    if (num == 1) {
+      this.prebuiltClassifier1Selected = true;
+    }
+    else if (num == 2) {
+      this.prebuiltClassifier2Selected = true;
+    }
+    else if (num == 3) {
+      this.prebuiltClassifier3Selected = true;
+    }
+    else if (num == 4) {
+      this.prebuiltClassifier4Selected = true;
+    }
+    else if (num == 5) {
+      this.prebuiltClassifier5Selected = true;
+    }
+    else if (num == 6) {
+      this.prebuiltClassifier6Selected = true;
+    }
+    else if (num == 9) {
+      this.prebuiltClassifier9Selected = true;
+    }
   }
+  public get WhatDescriptionToDisplay(): string {
+    return "highlighted text";
+  }
+
   //subscribe below
 
   public ApplyCode: boolean = false;
@@ -672,7 +724,7 @@ export class SearchComp implements OnInit, OnDestroy {
       }
       if (this.modelNum == 1) {
         this.modelTitle = 'RCT';
-        this.ModelId = -1;
+        this.ModelId = -1;        
       } else if (this.modelNum == 2) {
         this.modelTitle = 'Systematic review';
         this.ModelId = -2;
@@ -1460,3 +1512,4 @@ export interface ReadOnlySource {
   duplicates: number;
   isDeleted: boolean;
 }
+
